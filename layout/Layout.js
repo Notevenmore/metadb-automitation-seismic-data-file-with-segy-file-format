2276ecc8d758000a1b9f739400beaccbdce21f93
vynsss <vickyvanessa02@yahoo.com>
2023-02-03 13:19:08 +0700

    prettier lol

--- layout/Layout.js
+++ layout/Layout.js
@@ -1,24 +1,25 @@
-import { useState } from 'react'
-import Footer from '../components/footer/Footer'
-import SideBar from '../components/navigation_bar/SideBar'
-import TopBar from '../components/navigation_bar/Topbar'
-import Styles from '../styles/Layout.module.css'
+import { useState } from "react";
+import Footer from "../components/footer/Footer";
+import SideBar from "../components/navigation_bar/SideBar";
+import TopBar from "../components/navigation_bar/Topbar";
+import Styles from "../styles/Layout.module.css";
 
-export default function Layout({children}) {
-    const [sideBar, setSideBar] = useState(true)
+export default function Layout({ children }) {
+    const [sideBar, setSideBar] = useState(true);
 
-    function toggleNavBar(){
-        setSideBar(prevToggle => !prevToggle)
-        console.log(sideBar)
+    function toggleNavBar() {
+        setSideBar((prevToggle) => !prevToggle);
+        console.log(sideBar);
     }
 
     return (
         <div className={sideBar && Styles.sideBarOpen}>
             <TopBar handleClick={toggleNavBar}></TopBar>
             <div className={Styles.sideBar}>
-            <SideBar></SideBar></div>
+                <SideBar></SideBar>
+            </div>
             <>{children}</>
             <Footer></Footer>
         </div>
-    )
+    );
 }
