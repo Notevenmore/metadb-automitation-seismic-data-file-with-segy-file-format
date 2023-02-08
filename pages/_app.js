54349413fb0c586baebcea8fc7d02fc783c679ac
Leon <doncorleone142857@gmail.com>
2023-02-08 15:40:17 +0700

    removed some imports from app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,9 @@
 import "../styles/globals.css";
-import LayoutIcon from "../layout/LayoutIcon";
-import LayoutCollapse from "../layout/LayoutCollapse";
 
 function MyApp({ Component, pageProps }) {
     return (
-        <LayoutIcon>
           <Component {...pageProps} />
-        </LayoutIcon>
+        
         // <LayoutCollapse>
         //     <Component {...pageProps} />
         // </LayoutCollapse>
