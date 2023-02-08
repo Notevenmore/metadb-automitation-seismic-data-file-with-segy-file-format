57606662b3caedfd264b073227758de39138ae0f
Leon <doncorleone142857@gmail.com>
2023-02-08 15:14:10 +0700

    deleted some components

--- components/footer/Footer.js
+++
@@ -1,7 +0,0 @@
-export default function Footer() {
-    return (
-        <footer className="w-full flex items-center border-t-2 border-slate-100 place-content-center py-2">
-            heyy
-        </footer>
-    )
-}
