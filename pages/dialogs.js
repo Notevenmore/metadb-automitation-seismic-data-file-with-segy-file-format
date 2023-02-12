9b1094fbf28b55fc76e9cf23ee17a8e0617645ff
Leon <doncorleone142857@gmail.com>
2023-02-13 04:16:25 +0700

    renamed file from index2.js to dialogs.js

--- pages/dialogs.js
+++ pages/dialogs.js
@@ -1,8 +1,6 @@
 import FloatDialogNotification, {FloatDialogProfile} from "../components/float_dialog"
 import PopupDialog from '../components/popup_dialog'
 
-import styles from '../styles/Home.module.css'
-
 export default function Home() {
   return (
     <div>
