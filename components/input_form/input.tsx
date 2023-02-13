44545f94f471e7f6e29bedaec03c2c9de3e5ae20
karelbondan <karel.herdito@binus.ac.id>
2023-02-14 00:27:27 +0700

    migrated some components to typescript

---
+++ components/input_form/input.tsx
@@ -0,0 +1,63 @@
+import { useEffect, useState } from 'react'
+import { twMerge } from 'tailwind-merge'
+import Arrow from '../../public/icons/arrow_notrail.svg'
+/**
+ * This component is meant to be used for text inputs only:
+ * inline-text, e-mail, and password. 
+ * 
+ * @param label string 
+ */
+
+interface InputProps extends React.ComponentProps<"input"> {
+    label: "";
+    label_loc: "";
+    dropdown_items: [];
+    additional_styles_label: "";
+    additional_styles_input: "";
+    additional_styles_menu_container: "";
+    additional_styles: "";
+
+}
+
+const Input: React.FunctionComponent<InputProps> = ({ label = "none", label_loc = "none", type, dropdown_items = [], additional_styles_label = '', additional_styles_input = '', additional_styles_menu_container = '', additional_styles = '', ...inputProps }) => {
+    const [Selected, setSelected] = useState()
+    const [CurrentlyFocused, setCurrentlyFocused] = useState<Element>()
+    const handleUnfocus = (e) => {
+        e.preventDefault()
+        if (document.activeElement === CurrentlyFocused) {
+            (document.activeElement as HTMLElement).blur()
+            setCurrentlyFocused(null)
+        } else {
+            setCurrentlyFocused(document.activeElement)
+        }
+    }
+    return (
+        <div className={twMerge(`${label_loc.toLowerCase() === "beside" ? "flex items-center space-x-2" : label_loc.toLowerCase() === "above" ? "flex flex-col items-start" : ""}`, additional_styles)}>
+            <label className={twMerge(`${label.toLowerCase() !== "none" ? "block" : "hidden"} w-[45%]  border-black`, additional_styles_label)}>
+                {label}
+            </label>
+            {type.toLowerCase() !== "dropdown" ?
+                <input
+                    className={`rounded-md bg-gray-200 placeholder:text-gray-500 outline-none px-2 py-1.5 w-full hover:bg-gray-300 focus:bg-gray-300 focus:outline-[2px] focus:outline-gray-400 transition-all ${additional_styles_input}`}
+                    {...inputProps}
+                />
+                :
+                <div tabIndex={0} className="group relative select-none w-full" onClick={handleUnfocus} onBlur={e => { setCurrentlyFocused(null) }}>
+                    <div className={twMerge(`flex justify-between items-center rounded-md bg-gray-200 placeholder:text-gray-500 outline-none px-2 py-1.5 w-full hover:bg-gray-300 focus:bg-gray-300 focus:outline-[2px] focus:outline-gray-400 transition-all`, additional_styles_input)}>
+                        <label className='truncate max-w-[80%]'>{Selected ? Selected : "Select an item"}</label>
+                        <Arrow className="w-2.5 rotate-90" />
+                    </div>
+                    <div className={twMerge(`hidden group-focus:block z-[50] absolute bg-gray-200 shadow-md mt-1 overflow-x-hidden overflow-y-auto left-0 rounded-md w-full min-h-[3px]`, additional_styles_menu_container)}>
+                        <ul className='list-none max-h-[230px]'>
+                            {dropdown_items.map((item, index) => {
+                                return (<li key={index} className='hover:bg-gray-300 py-1 px-2 transition-all' onClick={e => { setSelected(item); (document.activeElement as HTMLElement).blur() }}>{item}</li>)
+                            })}
+                        </ul>
+                    </div>
+                </div>
+            }
+        </div>
+    )
+}
+
+export default Input
---
+++ components/table/table.tsx
@@ -0,0 +1,90 @@
+import { twMerge } from "tailwind-merge"
+import { useState, useEffect, memo } from "react"
+
+
+const TableComponent = ({ header, content, with_checkbox = false, additional_styles = '', setSelectedRows }) => {
+  const [Selected, setSelected] = useState([])
+  const tableData = {
+    header: header,
+    content: content
+  }
+
+  const handleSelectAll = (e) => {
+    const temp = []
+    const checkbox = document.getElementById("checkbox_all") as HTMLInputElement
+    const checkbox_all = document.getElementsByName("checkbox_row")
+    checkbox_all.forEach((check: HTMLInputElement) => {
+      if (checkbox.checked) {
+        check.checked = true
+        temp.push(check.parentElement.parentElement.getAttribute("id"))
+      } else {
+        check.checked = false
+      }
+    })
+    setSelected(temp)
+  }
+  const handleSelectRow = (e, row_input_id: string, row_id: string) => {
+    let temp = Selected
+    const checkbox_header = document.getElementById("checkbox_all") as HTMLInputElement
+    const checkbox_row = document.getElementById(row_input_id) as HTMLInputElement
+    if (checkbox_header.checked && !checkbox_row.checked) {
+      checkbox_header.checked = false
+    }
+    if (!checkbox_row.checked) {
+      temp = temp.filter(item => item !== row_id)
+      setSelected(temp)
+    }
+    else {
+      setSelected(prev => ([...prev, row_id]))
+    }
+  }
+  useEffect(() => {
+    if (with_checkbox) {
+      // directly using the setState function is prevented for performance sake 
+      // (to prevent re-render, which is very expensive in resource in this case)
+      setSelectedRows[0] = Selected
+      // console.log(setSelectedRows[0])
+      // console.log(Selected)
+    }
+  }, [Selected])
+
+  return (
+    <table className={twMerge('table-fixed break-words overflow-x-scroll border-separate border-spacing-0 border-2 min-w-0 w-full rounded-lg text-[15px]', additional_styles)}>
+      <thead className="bg-gray-200">
+        <tr className="text-left">
+          {with_checkbox ? <th className="pl-[14px] pt-1 w-5"><input type="checkbox" id="checkbox_all" onClick={(e) => handleSelectAll(e)} /></th> : null}
+          {tableData.header.map((header, index) => {
+            return (
+              <th key={index} className="pl-5 pr-2 py-2">{header}</th>
+            )
+          })}
+        </tr>
+      </thead>
+      <tbody className="text-ellipsis break-words">
+        {tableData.content.map((row, row_index) =>
+          <tr key={row_index} id={'row_' + row_index} className="hover:bg-side_bar">
+            {with_checkbox ? <td className="pl-[14px] pt-1 w-5 border-t-2"><input id={'row_input_' + row_index} name="checkbox_row" type="checkbox" onClick={(e) => handleSelectRow(e, "row_input_" + row_index, "row_" + row_index)} /></td> : null}
+            {typeof row === 'object' && !Array.isArray(row) && row !== null ?
+              Object.values(row).map((column: any, column_index: number) => {
+                return (
+                  <td key={column_index} className="pl-5 pr-2 py-2 border-t-2">
+                    <div className="whitespace-nowrap text-ellipsis overflow-hidden">{column}</div>
+                  </td>
+                )
+              })
+              :
+              row.map((column: any, column_index: number) => {
+                return (
+                  <td key={column_index} className="pl-5 pr-2 py-2 border-t-2">
+                    <div className="whitespace-nowrap text-ellipsis overflow-hidden">{column}</div>
+                  </td>
+                )
+              })
+            }
+          </tr>)}
+      </tbody>
+    </table>
+  )
+}
+
+export default TableComponent
