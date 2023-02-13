511a474194f5935a32193b122b396fe6c3a6d234
karelbondan <karel.herdito@binus.ac.id>
2023-02-14 00:25:15 +0700

    updated footer

--- components/footer/Footer.js
+++ components/footer/Footer.js
@@ -1,7 +1,7 @@
 export default function Footer() {
     return (
         <footer className="w-full flex items-center border-t-2 border-slate-100 place-content-center py-2">
-            heyy
+            <p className="text-black/[.3] text-xs">© Kangean Energy Indonesia 2023</p>
         </footer>
     )
 }
