1f6285d32359f12081678ace41920986b70a54de
Leon <doncorleone142857@gmail.com>
2023-02-15 21:35:09 +0700

    refactored popup dialog and added documentation

--- components/popup_dialog/popup_dialog.js
+++ components/popup_dialog/popup_dialog.js
@@ -1,38 +1,49 @@
-import {twMerge} from "tailwind-merge"
 import Buttons from "../buttons"
 import Close from "../../public/icons/close.svg"
-import {FloatDivider as Divider} from "../float_dialog/float_dialog"
+import {Divider} from "../float_dialog/float_dialog"
 
-const PopupDialog = () => {
+const PopupDialog = ({title="Are you sure?", content,
+     button_desc1="Save", button_desc2="Save and exit",
+      button_desc3="Discard changes"}) => 
+{
+    const DEFAULT_CONTENT = () => {
+        return (
+            <>
+            You have not saved the document yet, are you sure you want to leave this page?
+            <strong> This document will not be uploaded and will be deleted.</strong>            
+            </>
+        )
+    }
     return (
         <aside className="flex flex-col justify-between w-[470px] h-[280px]
          px-[26px] py-[24px] border-[1px] border-solid border-[#C9C9C9] rounded-[10px]">
-            <PopupTitle></PopupTitle>
+            <PopupTitle title={title}></PopupTitle>
             <Divider additional_styles={"-ml-[27px] w-[470px]"}></Divider>
-            <p className="text-[16px] w-[397px] text-center">You have not saved the document yet, are you sure you want to leave this page?
-                <strong>This document will not be uploaded and will be deleted.</strong>
-            </p>
-            <ButtonsSection></ButtonsSection>
+            <p className="text-[16px] w-[397px] text-center">{content ? content : DEFAULT_CONTENT()}</p>
+            <ButtonsSection
+             button_desc1={button_desc1}
+             button_desc2={button_desc2}
+             button_desc3={button_desc3}/>
         </aside>
     )
 }
 
-const PopupTitle = () => {
+const PopupTitle = ({title}) => {
     return (
         <section className="flex flex-row justify-between">
-            <h2 className="text-[18px]"><strong>Are you sure?</strong></h2>
+            <h2 className="text-[18px]"><strong>{title}</strong></h2>
             <div className="flex bg-[#D9D9D9] w-[27px] h-[27px] rounded-[5px]">
                 <Close className="my-auto mx-auto h-[13px] w-[13px]"></Close>
             </div>
         </section>    
     )
 }
-const ButtonsSection = () => {
+const ButtonsSection = ({button_desc1="Save", button_desc2="Save and exit", button_desc3="Discard changes"}) => {
     return (
         <section className="flex flex-row justify-around">
-            <Buttons path='/' button_description='Save' additional_styles={"bg-[#63F48C]"}/>
-            <Buttons path='/' button_description='Save and exit' additional_styles={"bg-[#63F48C]"}/>
-            <Buttons path='/' button_description='Discard changes' additional_styles={"text-[#FF0000]"}/>
+            <Buttons path='/' button_description={button_desc1} additional_styles="bg-[#63F48C]" children="<h1>Hello<h1>"/>
+            <Buttons path='/' button_description={button_desc2} additional_styles="bg-[#63F48C]"/>
+            <Buttons path='/' button_description={button_desc3} additional_styles="text-[#FF0000]"/>
         </section>
     )
 }
--- pages/popup-dialog.js
+++ pages/popup-dialog.js
@@ -1,9 +1,120 @@
+import hljs from 'highlight.js'
+import { useEffect } from 'react'
+import Highlight from 'react-highlight'
 import PopupDialog from '../components/popup_dialog/popup_dialog'
 
 export default function PopupDialogPage() {
+  useEffect(() => {
+    hljs.highlightAll()
+  }, [])
   return (
-    <div>
-      <PopupDialog></PopupDialog>
-    </div>
+    <section className="flex flex-col w-full h-fit text-[14.5px]
+     border-[2px] border-solid border-black px-10 py-5">
+      <HeaderSection></HeaderSection>
+      <MainSection></MainSection>
+    </section>
   )
 }
+
+const HeaderSection = () => {
+  return (
+    <header className="flex flex-col space-y-2 mb-[2em]">
+      <h1 className='text-4xl font-bold'>Popup Dialog Component</h1>
+      <p>The <code><strong>PopupDialog</strong></code> component consists of no props,
+      since it is intended to be used as a final component. <br></br>
+        Below is the example of usage of the PopupDialog component.
+      </p>
+    </header> 
+  )
+}
+
+const MainSection = () => {
+  return (
+    <main className="flex flex-col">
+      <Example1></Example1>
+      <Example2></Example2>
+      <Example3></Example3>
+    </main>     
+  )
+}
+const Example = (props) => {
+  return (
+    <section className="flex flex-col gap-y-[20px] mb-[20px]">
+      {props.children}
+    </section>
+  )
+}
+const Example1 = () => {
+  return (
+    <Example>
+      <h3 className='text-xl'><strong>Example 1: Popup dialog in the center of the screen</strong></h3>
+      <Highlight className='html rounded-md border-solid border-black border-2 break-words w-[500px] overflow-y-scroll'>
+      {
+`<div className="flex justify-center items-center
+  border-[2px] border-solid border-black h-[490px]">
+  <PopupDialog></PopupDialog>
+</div>`
+      }
+      </Highlight>
+      <div className="flex justify-center items-center border-[2px] border-solid border-black h-[490px]">
+        <PopupDialog
+          title={"hello"}
+          // content={
+          //   <>You have not saved the document yet, are you sure you want to leave this page?
+          //   <strong> This document will not be uploaded and will be deleted.</strong></> 
+          // }
+        />
+      </div>
+    </Example>
+    // <section className="flex flex-col gap-y-[20px] mb-[20px]">
+    // </section>
+  )    
+}
+
+const Example2 = () => {
+  return (
+    <Example>
+      <h3 className='text-xl'><strong>Example 2: Popup dialog in the top-left of the screen</strong></h3>
+      <Highlight className='html rounded-md border-solid border-black border-2 break-all w-[500px] overflow-y-scroll'>
+      {
+`<div className="flex border-[2px]
+border-solid border-black h-[490px]">
+  <PopupDialog></PopupDialog>
+</div>`
+      }
+      </Highlight>
+      <div className="flex border-[2px] border-solid border-black h-[490px]">
+        <PopupDialog
+        content={
+          <>You have not saved the document yet, are you sure you want to leave this page?
+          <strong> This document will not be uploaded and will be deleted.</strong></>
+        }
+        />
+      </div>
+    </Example>
+  )
+}
+
+const Example3 = () => {
+  return (
+    <Example>
+      <h3 className='text-xl'><strong>Example 3: Popup dialog in the bottom-right of the screen</strong></h3>
+      <Highlight className='html rounded-md border-solid border-black border-2 break-all w-[500px] overflow-y-scroll'>
+      {
+`<div className="flex justify-end items-end
+border-[2px] border-solid border-black h-[490px]">
+  <PopupDialog></PopupDialog>
+</div>`
+      }
+      </Highlight>
+      <div className="flex justify-end items-end border-[2px] border-solid border-black h-[490px]">
+        <PopupDialog
+          content={
+            <>You have not saved the document yet, are you sure you want to leave this page?
+            <strong> This document will not be uploaded and will be deleted.</strong></>
+        }
+          />
+      </div>
+    </Example>
+  )
+} 
