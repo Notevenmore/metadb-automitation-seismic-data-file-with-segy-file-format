15eaab5e72b490a14407720f18c1f7f55a35c904
vynsss <vickyvanessa02@yahoo.com>
2023-02-17 22:44:17 +0700

    modify documentation

--- pages/components/container.js
+++ pages/components/container.js
@@ -12,7 +12,7 @@ export default function ContainerPage() {
 				</p>
 				<h3 className="text-xl font-bold">Container</h3>
 				<p>This act as a container</p>
-				<Highlight className="html rounded-md border-2">{"<Container> ... </Container>"}</Highlight>
+				<Highlight className="html rounded-md border-2">{"<Container> <div></div> </Container>"}</Highlight>
 				<h3 className="text-xl font-bold">Container.Title</h3>
 				<p>This container will have a title. Props:</p>
 				<ul className="list-disc px-10">
@@ -22,14 +22,14 @@ export default function ContainerPage() {
 				</ul>
                 <p className="font-bold">Code:</p>
 				<Highlight className="html rounded-md border-2">
-					{"<Container> \n\t<Container.Title>Title</Container.Title> \n</Container>"}
+					{"<Container> \n\t<Container.Title>Title</Container.Title> \n\t<div></div> \n</Container>"}
 				</Highlight>
                 <p className="font-bold">Output:</p>
                 <Container.Title>Title</Container.Title>
                 <p>Adding a <code>back</code> as seen below, the title now will have a back button on the right side of the title which can be used to go back to previous page</p>
                 <p className="font-bold">Code:</p>
                 <Highlight className="html rounded-md border-2">
-					{"<Container> \n\t<Container.Title back>Title</Container.Title> \n</Container>"}
+					{"<Container> \n\t<Container.Title back>Title</Container.Title> \n\t<div></div> \n</Container>"}
 				</Highlight>
                 <p className="font-bold">Output:</p>
                 <Container.Title back>Title Back</Container.Title>
@@ -45,7 +45,7 @@ export default function ContainerPage() {
                 <p className="font-bold">Code:</p>
 				<Highlight className="html rounded-md border-2">
 					{
-						"<Container> \n\t<Container.Title>Title</Container.Title> \n\t<Container.Subtitle>subtitle</Container.Subtitle> \n</Container>"
+						"<Container> \n\t<Container.Title>Title</Container.Title> \n\t<Container.Subtitle>subtitle</Container.Subtitle> \n\t<div></div> \n</Container>"
 					}
 				</Highlight>
                 <p className="font-bold">Output:</p>
@@ -57,7 +57,7 @@ export default function ContainerPage() {
                 <Container.Subtitle>Subtitle here</Container.Subtitle>
                 <Highlight className="html rounded-md border-2">
 					{
-						"<Container> \n\t<Container.Title back>Title</Container.Title> \n\t<Container.Subtitle tab>subtitle</Container.Subtitle> \n</Container>"
+						"<Container> \n\t<Container.Title back>Title</Container.Title> \n\t<Container.Subtitle tab>subtitle</Container.Subtitle> \n\t<div></div> \n</Container>"
 					}
 				</Highlight>
                 <p className="font-bold">Output:</p>
