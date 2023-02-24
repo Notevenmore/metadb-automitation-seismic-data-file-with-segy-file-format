2e685cd0e185f8e4fa30a388aa96dc8e7b9e516e
vynsss <vickyvanessa02@yahoo.com>
2023-02-24 23:08:11 +0700

    create search widget on sidebar

---
+++ components/widget/Search.js
@@ -0,0 +1,9 @@
+export default function Search() {
+    const [expand, setExpand] = useState(false);
+    
+    return (
+        <div className="w-full h-auto">
+            
+        </div>
+    )
+}
