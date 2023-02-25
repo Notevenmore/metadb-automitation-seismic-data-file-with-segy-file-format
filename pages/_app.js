9c135159b9366ea4c012d37a170c3cfc9f42d4e1
vynsss <vickyvanessa02@yahoo.com>
2023-02-25 15:28:00 +0700

    modify to make more proportional

--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,23 @@
 import "../styles/globals.css";
 import "highlight.js/styles/github.css";
-import { getLayoutIcon } from "../layout/getLayout";
+import { getLayoutIcon, getLayoutWidget } from "../layout/getLayout";
 import { store } from "../store";
 import { Provider } from "react-redux";
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }) {
-	const getLayout = Component.getLayout || getLayoutIcon;
+	const getLayout = Component.getLayout || getLayoutWidget;
 
-	return <Provider store={store}>{getLayout(<Component {...pageProps} />)}</Provider>;
+	return (
+		<Provider store={store}>
+			<Head>
+				<title>Profile - Kangean Energy Indonesia Database Converter App</title>
+				<meta name="description" content="Kangean Energy Indonesia Database Converter App" />
+				<link rel="icon" href="/icons/kangean_logo.svg" />
+			</Head>
+			{getLayout(<Component {...pageProps} />)}
+		</Provider>
+	);
 }
 
 export default MyApp;
--- pages/profile.js
+++ pages/profile.js
@@ -1,45 +1,30 @@
-import Buttons from '../components/buttons/buttons'
-import Arrow from '../public/icons/arrow_notrail.svg'
-import Table from '../components/table/table'
-import Head from 'next/head'
-import TableComponent from '../components/table/table'
+import Buttons from "../components/buttons/buttons";
+import TableComponent from "../components/table/table";
+import Container from "../components/container/container";
 
 const Profile = () => {
-    return (
-        <div className="h-full py-5 px-10 space-y-5 text-[14.5px]">
-            <Head>
-                <title>Profile - Kangean Energy Indonesia Database Converter App</title>
-                <meta name='description' content='Kangean Energy Indonesia Database Converter App' />
-                <link rel='icon' href='/icons/kangean_logo.svg' />
-            </Head>
-            <div className='flex space-x-5 py-2 items-center'>
-                <Buttons path='/' button_description='' additional_styles='px-4 py-4' >
-                    <Arrow className='rotate-180 w-3 h-3' />
-                </Buttons>
-                <h2 className='text-3xl font-semibold'>Account settings</h2>
-            </div>
-            <TableComponent
-                header={["Account information", ""]}
-                content={[
-                    ["Email", "john.doe@email.com",],
-                    ["Date joined", "01 - 01 - 2023"],
-                    ["Role", "IT Admin"],
-                ]}
-                additional_styles='text-[14.5px]'
-            />
-            <div>
-                <h3 className='font-semibold'>Password</h3>
-                <p>
-                    Reset your password by clicking the link below. The password reset confirmation
-                    will be sent to your email.
-                </p>
-            </div>
-            <Buttons
-                path=''
-                button_description='Change my password'
-            />
-        </div>
-    )
-}
+	return (
+		<Container>
+			<Container.Title back>Account settings</Container.Title>
+			<TableComponent
+				header={["Account information", ""]}
+				content={[
+					["Email", "john.doe@email.com"],
+					["Date joined", "01 - 01 - 2023"],
+					["Role", "IT Admin"],
+				]}
+				additional_styles="text-[14.5px]"
+			/>
+			<div>
+				<h3 className="font-semibold">Password</h3>
+				<p>
+					Reset your password by clicking the link below. The password reset confirmation will be sent to your
+					email.
+				</p>
+			</div>
+			<Buttons path="" button_description="Change my password" />
+		</Container>
+	);
+};
 
-export default Profile
+export default Profile;
