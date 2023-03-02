518b4ea2e2c12da0467c81d254d3b878b3efcbc0
Leon <doncorleone142857@gmail.com>
2023-03-03 00:57:02 +0700

    did some refactoring for best practice

--- components/container/container.js
+++ components/container/container.js
@@ -1,4 +1,5 @@
 import { useRouter } from "next/router"
+import Image from "next/image"
 
 export default function Container({children, additional_class, ...divProps}) {
     return(
@@ -9,19 +10,28 @@ export default function Container({children, additional_class, ...divProps}) {
 }
 
 Container.Title = ({children, back=false}) => {
-    const router = useRouter();
     return (
     <div className="flex flex-row items-center text-[36px] mb-10 gap-x-5 w-full">
-        {back && <div className="bg-[#d9d9d9]/[49%] rounded w-[35px] h-[35px] flex justify-center items-center" onClick={() => router.back()}>
-            <img src="/icons/chevron-left.svg" className="w-[25px] h-[25px]" />
-        </div>}
+        {back && <BackButton />}
         <div className="font-bold flex-1">
             {children}
         </div>
     </div>
 )}
+Container.Title.displayName = 'Container.Title';
+
+function BackButton() {
+    const router = useRouter();
+    return (
+        <div className="hover:bg-black/[0.15] rounded w-[50px] h-[35px] flex justify-center items-center" onClick={() => router.back()}>
+            <Image src="/icons/chevron-left.svg" alt="back icon" width={35} height={35} />
+        </div>
+      
+    );
+  }
 
 Container.Subtitle = ({children, additional_class, tab=false}) => {
+    Container.Subtitle.displayName = 'Container.Subtitle';
     return (
         <div className={`text-[12px] -mt-10 lg:text-[16px] mb-10 ${additional_class}`} style={{marginLeft: tab && "58px"}}>
             {children}
