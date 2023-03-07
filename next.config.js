6c81d18214b4e01f0dba36ae5fcd81cc09db971c
vynsss <vickyvanessa02@yahoo.com>
2023-03-07 21:39:14 +0700

    set env for backend url

--- next.config.js
+++ next.config.js
@@ -1,5 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  env: {
+    backend_url: "http://localhost:8080/api/v1"
+  },
   reactStrictMode: true,
   webpack(config){
     config.module.rules.push({
