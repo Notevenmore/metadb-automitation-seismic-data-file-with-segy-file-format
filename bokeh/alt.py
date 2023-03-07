6e1c32641433b0e8cd19c7bf95deb219777b1e1d
vynsss <vickyvanessa02@yahoo.com>
2023-03-07 21:38:52 +0700

    tryout bokeh

--- bokeh/alt.py
+++ bokeh/alt.py
@@ -14,28 +14,72 @@ bop.reset_output()
 
 x = np.linspace(0, np.pi)
 y = np.sin(x)
-source = ColumnDataSource(data=dict(x=x, y=y))
+# source = ColumnDataSource(data=dict(x=x, y=y))
 
-freq = Slider(name="Frequency", start=0, end=10, value=2)
-phase = Slider(name="Phase", start=0, end=np.pi)
+def sine(source=source):
+    # source = ColumnDataSource(data=dict(x=x, y=y))
+    freq = Slider(name="Frequency", start=0, end=10, value=2)
+    callback = CustomJS(args=dict(source=source, val=freq),
+                        code="""
+        const data = source.data;
+        const freq = val.value;
+        const x = data['x'];
+        const y = data['y'];
+        for (var i = 0; i < x.length; i++) {
+            y[i] = Math.sin(freq*x[i]);
+        }
+        console.log(y)
+        source.change.emit()
+    """)
+    freq.js_on_change('value', callback)
 
-callback = CustomJS(args=dict(source=source, val=freq),
-                    code="""
-    const data = source.data;
-    const freq = val.value;
-    const x = data['x'];
-    const y = data['y'];
-    for (var i = 0; i < x.length; i++) {
-        y[i] = Math.sin(freq5*x[i]);
-    }
-    console.log(y)
-    source.change.emit()
-""")
-freq.js_on_change('value', callback)
-
-def sine():
-    bop.reset_output()
     p = figure()
     p.line('x', 'y', source = source, line_width=3, line_alpha=0.5)
     layout = row(column(freq, width=100), p)
     return json_item(layout, "plot")
+
+def cosine():
+    source = ColumnDataSource(data=dict(x=x, y=y))
+    freq = Slider(name="Frequency", start=0, end=10, value=2)
+    callback = CustomJS(args=dict(source=source, val=freq),
+                        code="""
+        const data = source.data;
+        const freq = val.value;
+        const x = data['x'];
+        const y = data['y'];
+        for (var i = 0; i < x.length; i++) {
+            y[i] = Math.cos(freq*x[i]);
+        }
+        console.log(y)
+        source.change.emit()
+    """)
+    freq.js_on_change('value', callback)
+
+    p = figure()
+    p.line('x', 'y', source = source, line_width=3, line_alpha=0.5)
+    layout = row(column(freq, width=100), p)
+    return json_item(layout, "plot")
+
+def tan():
+    source = ColumnDataSource(data=dict(x=x, y=y))
+    freq = Slider(name="Frequency", start=0, end=10, value=2)
+    callback = CustomJS(args=dict(source=source, val=freq),
+                        code="""
+        const data = source.data;
+        const freq = val.value;
+        const x = data['x'];
+        const y = data['y'];
+        for (var i = 0; i < x.length; i++) {
+            y[i] = Math.cos(freq*x[i]);
+        }
+        console.log(y)
+        source.change.emit()
+    """)
+    freq.js_on_change('value', callback)
+
+    p = figure()
+    p.line('x', 'y', source = source, line_width=3, line_alpha=0.5)
+    layout = row(column(freq, width=100), p)
+    return json_item(layout, "plot")
+
+# def allFunc():
