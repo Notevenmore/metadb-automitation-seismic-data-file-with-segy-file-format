f995467f87455a7b05f950b7bfc1e925197c0f48
vynsss <vickyvanessa02@yahoo.com>
2023-03-08 20:52:19 +0700

    create mime function

--- components/navigation_bar/Topbar.js
+++ components/navigation_bar/Topbar.js
@@ -3,8 +3,18 @@ import { mdiBellOutline, mdiHomeFloorA } from "@mdi/js";
 import RoundImage from "../image/RoundImage";
 import Kangean from '../../public/icons/kangean_logo.svg'
 import Link from "next/link"
+import { useEffect, useState } from "react";
+import Mime from "../../dummy-data/mime";
+import { useSelector } from "react-redux";
 
 export default function TopBar(props) {
+	const user = useSelector((state) => state.user.user);
+
+	const [profile, setProfile] = useState("")
+	useEffect(() => {
+		setProfile(Mime(user.profile_picture))
+	}, []);
+
     return (
         <nav className="w-full flex justify-between items-center py-2 px-5 border-b border-gray-300">
             <Link href="/" className="flex items-center gap-x-4">
@@ -15,7 +25,7 @@ export default function TopBar(props) {
                 <img src="/icons/bell-outline.svg" className="w-[1rem]" />
                 <div className="border-l-[1.5px] border-slate-200 h-6"> </div>
                 <RoundImage
-                    source="/images/unknown.jpg"
+                    source={profile}
                     size={{ width: "1.5rem" }}
                 ></RoundImage>
             </div>
--- pages/profile.js
+++ pages/profile.js
@@ -2,23 +2,19 @@ import Buttons from "../components/buttons/buttons";
 import TableComponent from "../components/table/table";
 import Container from "../components/container/container";
 import { useSelector } from "react-redux";
-import { checkAuth } from "../layout/getLayout";
 import { useEffect, useState } from "react";
-import Image from "next/image";
-import base64 from "base-64";
+import moment from "moment/moment";
 
 const Profile = () => {
 	const user = useSelector((state) => state.user.user);
-	console.log("???");
 
 	const [content, setContent] = useState([])
 	useEffect(() => {
 		setContent([
 			["Email", user.email],
-			["Date joined", user.date_joined],
+			["Date joined", moment(user.date_joined).format("DD - MM - YYYY")],
 			["Role", user.role_id],
 		])
-		console.log(base64.decode(user.profile_picture));
 	}, []);
 
 	return (
@@ -38,9 +34,6 @@ const Profile = () => {
 					</p>
 				</div>
 				<Buttons path="" button_description="Change my password" />
-				<div className="h-[200px] w-[200px]">
-					<Image src="" width={100} height={100} className="object-fit" />
-				</div>
 			</div>
 		</Container>
 	);
