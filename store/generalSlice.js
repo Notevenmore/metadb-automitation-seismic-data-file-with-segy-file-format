8e12bca11ab6c9348b7ab75b2bb523984cd8b4ed
karelbondan <karel.herdito@binus.ac.id>
2023-04-19 18:42:34 +0700

    added redux states

--- store/generalSlice.js
+++ store/generalSlice.js
@@ -1,19 +1,27 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
-    file: []
+    file: [],
+    document_summary: null,
+    review_data: null
 }
 
 export const generalSlice = createSlice({
-    name:"general",
+    name: "general",
     initialState,
     reducers: {
         storeFile: (state, action) => {
             console.log(action.payload)
             state.file = action.payload
+        },
+        setDocumentSummary: (state, action) => {
+            state.document_summary = action.payload
+        },
+        setReviewData: (state, action) => {
+            state.review_data = action.payload
         }
     }
 })
 
-export const {storeFile} = generalSlice.actions
+export const { storeFile, setDocumentSummary, setReviewData } = generalSlice.actions
 export default generalSlice.reducer;
