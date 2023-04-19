ff664ce474d563802831d75659584d500e529c92
karelbondan <karel.herdito@binus.ac.id>
2023-04-19 18:47:45 +0700

    visual adjustments and supporting function for demo purposes

--- pages/well_data/printed_report.js
+++ pages/well_data/printed_report.js
@@ -5,17 +5,23 @@ import Input from "../../components/input_form/input";
 import TableComponent from "../../components/table/table";
 import Link from "next/link";
 import { useRouter } from "next/router";
+import Button from "../../components/buttons/buttons";
 
 const PrintedWellReport = ({ setTitle }) => {
     setTitle("Printed Well Report")
     const router = useRouter()
     const path_query = "Home" + router.pathname.replace(/\//g, " > ").replace(/\_/g, " ")
     let selectedTableData = [[]];
+    
+    const handleEditClick = (e, workspace_name) => {
+        let final = workspace_name.toLocaleLowerCase().replace(/\s/g, '_')
+        router.push(`/edit/${final}`)
+    }
+
     let table_data = [
-        { No: 1, Name: "Laporan Data 2023", KKS: "Geodwipa Teknika Nusantara", "wilayah kerja": "Jakarta", jenis: "Printed well report", AFE: "2893728901", action: <div className="flex flex-row gap-x-1 items-center"><Image src="/icons/magnify.svg" width={50} height={50} className="w-[25px] h-[15px] alt='' " alt="icon" /><Link href="/edit/test"><Image src="/icons/pencil.svg" width={50} height={50} className="w-[25px] h-[15px] alt='' " alt="icon" /></Link><Image src="/icons/delete.svg" width={50} height={50} className="w-[25px] h-[15px] alt='' " alt="icon" /></div> },
-        { No: 2, Name: "Workspace 2", KKS: "Geodwipa Teknika Nusantara", "wilayah kerja": "Jakarta", jenis: "Printed well report", AFE: "2022010201", action: <div className="flex flex-row gap-x-1 items-center"><Image src="/icons/magnify.svg" width={50} height={50} className="w-[25px] h-[15px] alt='' " alt="icon" /><Link href="/edit/test"><Image src="/icons/pencil.svg" width={50} height={50} className="w-[25px] h-[15px] alt='' " alt="icon" /></Link><Image src="/icons/delete.svg" width={50} height={50} className="w-[25px] h-[15px] alt='' " alt="icon" /></div> },
-        { No: 3, Name: "New workspace", KKS: "Geodwipa Teknika Nusantara", "wilayah kerja": "Jakarta", jenis: "Printed well report", AFE: "2023032801", action: <div className="flex flex-row gap-x-1 items-center"><Image src="/icons/magnify.svg" width={50} height={50} className="w-[25px] h-[15px] alt='' " alt="icon" /><Link href="/edit/test"><Image src="/icons/pencil.svg" width={50} height={50} className="w-[25px] h-[15px] alt='' " alt="icon" /></Link><Image src="/icons/delete.svg" width={50} height={50} className="w-[25px] h-[15px] alt='' " alt="icon" /></div> },
-        { No: 4, Name: "New workspace from uploaded file", KKS: "Geodwipa Teknika Nusantara", "wilayah kerja": "Jakarta", jenis: "Printed well report", AFE: "2023032802", action: <div className="flex flex-row gap-x-1 items-center"><Image src="/icons/magnify.svg" width={50} height={50} className="w-[25px] h-[15px] alt='' " alt="icon" /><Link href="/edit/test"><Image src="/icons/pencil.svg" width={50} height={50} className="w-[25px] h-[15px] alt='' " alt="icon" /></Link><Image src="/icons/delete.svg" width={50} height={50} className="w-[25px] h-[15px] alt='' " alt="icon" /></div> },
+        { No: 1, Name: "Laporan Data 2023", KKS: "Geodwipa Teknika Nusantara", "wilayah kerja": "Jakarta", jenis: "Printed well report", AFE: "2893728901", action: <div className="flex flex-row gap-x-1 items-center"><Image src="/icons/magnify.svg" width={50} height={50} className="w-[25px] h-[15px] alt='' " alt="icon" /><Button title="Edit workspace" path="" className="" onClick={(e) => handleEditClick(e, 'Laporan Data 2023')}><Image src="/icons/pencil.svg" width={50} height={50} className="w-[25px] h-[15px] alt='' " alt="icon" /></Button><Image src="/icons/delete.svg" width={50} height={50} className="w-[25px] h-[15px] alt='' " alt="icon" /></div> },
+        { No: 2, Name: "Workspace 2", KKS: "Geodwipa Teknika Nusantara", "wilayah kerja": "Jakarta", jenis: "Printed well report", AFE: "2022010201", action: <div className="flex flex-row gap-x-1 items-center"><Image src="/icons/magnify.svg" width={50} height={50} className="w-[25px] h-[15px] alt='' " alt="icon" /><Button title="Edit workspace" path="" className="" onClick={(e) => handleEditClick(e, 'Workspace 2')}><Image src="/icons/pencil.svg" width={50} height={50} className="w-[25px] h-[15px] alt='' " alt="icon" /></Button><Image src="/icons/delete.svg" width={50} height={50} className="w-[25px] h-[15px] alt='' " alt="icon" /></div> },
+        { No: 3, Name: "New workspace from uploaded file", KKS: "Geodwipa Teknika Nusantara", "wilayah kerja": "Jakarta", jenis: "Printed well report", AFE: "2023032801", action: <div className="flex flex-row gap-x-1 items-center"><Image src="/icons/magnify.svg" width={50} height={50} className="w-[25px] h-[15px] alt='' " alt="icon" /><Button title="Edit workspace" path="" className="" onClick={(e) => handleEditClick(e, 'New workspace from uploaded file')}><Image src="/icons/pencil.svg" width={50} height={50} className="w-[25px] h-[15px] alt='' " alt="icon" /></Button><Image src="/icons/delete.svg" width={50} height={50} className="w-[25px] h-[15px] alt='' " alt="icon" /></div> },
     ]
     const [data, setData] = useState(table_data);
     const onSearch = (e) => {
@@ -32,7 +38,7 @@ const PrintedWellReport = ({ setTitle }) => {
             <Container.Title>
                 <div className="flex flex-row items-center justify-between">
                     <div>
-                        <p className="text-base font-normal capitalize">{path_query}</p>
+                        <p className="text-sm font-normal capitalize">{path_query}</p>
                         <p>Printed Well Report</p>
                     </div>
                     <div className="w-[80%] lg:w-[40%] relative">
@@ -40,22 +46,25 @@ const PrintedWellReport = ({ setTitle }) => {
                             label=""
                             type="text"
                             name="search"
-                            additional_styles_input="h-[25px] !rounded-full text-[12px]"
+                            additional_styles_input="px-4 rounded-full text-base"
                             additional_styles="flex flex-col items-center justify-center"
                             onChange={(e) => onSearch(e)}
+                            autoComplete="off"
+                            placeholder="Search workspace name"
                         />
                         <Image
                             src="/icons/magnify.svg"
                             width="20"
                             height="20"
-                            className="absolute right-[10px] top-[2.5px]"
+                            // className="absolute right-[10px] top-[2.5px]"
+                            className="absolute top-[50%] right-3 translate-y-[-50%]"
                             alt="search"
                         />
                     </div>
                 </div>
             </Container.Title>
             <TableComponent
-                header={["No", "Name", "KKS", "Wilayah Kerja", "Jenis", "AFE", "Action"]}
+                header={["No", "Name", "KKS", "Working area", "Type", "AFE", "Action"]}
                 content={data} setSelectedRows={selectedTableData} with_checkbox />
         </Container>
     )
