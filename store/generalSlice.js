262296f99e8273508093cec4a1a6278864d444d2
karelbondan <karel.herdito@binus.ac.id>
2023-04-25 00:42:30 +0700

    added support for ocr data and uploaded document summary

--- store/generalSlice.js
+++ store/generalSlice.js
@@ -15,9 +15,11 @@ export const generalSlice = createSlice({
             state.file = action.payload
         },
         setDocumentSummary: (state, action) => {
+            console.log(action.payload)
             state.document_summary = action.payload
         },
         setReviewData: (state, action) => {
+            console.log(action.payload)
             state.review_data = action.payload
         }
     }
