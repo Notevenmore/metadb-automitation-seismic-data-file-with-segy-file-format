f7f65ebb7b3ad753fa911d6436d820ea2dd265dc
karelbondan <karel.herdito@binus.ac.id>
2023-05-01 14:49:40 +0700

    added conditional statement in return to avoid undefined value

--- pages/edit/[edit].js
+++ pages/edit/[edit].js
@@ -77,51 +77,58 @@ const DocEditor = ({ spreadsheetID, workspace_name }) => {
     }, [iframe_ref])
 
     return (
-        <Container additional_class='space-y-3'>
-            <Container.Title back>Edit Document</Container.Title>
-            <Input type='text' placeholder='Document title' additional_styles_input='text-xl font-semibold p-3 capitalize' defaultValue={WorkspaceName} />
-            <TableComponent additional_styles_column="overflow-visible" header={["Header", ""]} content={
-                [
-                    [<div className="flex space-x-2"><p>Nama KKKS</p><p className="text-gray-400">(KKKS Name)</p></div>, <Input type={"text"} defaultValue="Geodwipa Teknika Nusantara" />],
-                    [<div className="flex space-x-2"><p>Nama wilayah kerja</p><p className="text-gray-400">(Working area)</p></div>, <Input type={"text"} defaultValue="Geodwipa Teknika Nusantara" />],
-                    [<div className="flex space-x-2"><p>Jenis penyerahan data</p><p className="text-gray-400">(Submission type)</p></div>, <Input type={"dropdown"} dropdown_items={["Quarterly", "Relinquishment", "Termination", "Spec New", "Spec Ext", "Spec Term", "Joint Study", "DIPA"]} />],
-                    [<div className="flex space-x-2"><p>Nomor AFE</p><p className="text-gray-400">(AFE number)</p></div>, <Input type={"number"} defaultValue='1' />],
-                    [<p className="font-bold">Data type</p>, <Input type={"dropdown"} dropdown_items={["Well data"]} />],
-                    [<p className="font-bold">Data classification</p>, <Input type={"dropdown"} dropdown_items={["Report"]} />],
-                    [<p className="font-bold">Data sub-classification</p>, <Input type={"dropdown"} dropdown_items={["Printed"]} />]
-                ]
-            } />
-            <div className="h-full">
-                <TableComponent header={[
-                    <div className="flex justify-between items-center">
-                        <p>Data</p>
-                    </div>
-                ]} content={[
-                    [<div className="h-[750px]"><Sheets type="review" form_type={router?.query.form_type || "basin"} data={Data} /></div>]
-                ]} additional_styles_row='p-0' additional_styles="overflow-hidden" />
+        (Data) ? (
+            <Container additional_class='space-y-3'>
+                <Container.Title back>Edit Document</Container.Title>
+                <Input type='text' placeholder='Document title' additional_styles_input='text-xl font-semibold p-3 capitalize' defaultValue={WorkspaceName} />
+                <TableComponent additional_styles_column="overflow-visible" header={["Header", ""]} content={
+                    [
+                        [<div className="flex space-x-2"><p>Nama KKKS</p><p className="text-gray-400">(KKKS Name)</p></div>, <Input type={"text"} defaultValue="Geodwipa Teknika Nusantara" />],
+                        [<div className="flex space-x-2"><p>Nama wilayah kerja</p><p className="text-gray-400">(Working area)</p></div>, <Input type={"text"} defaultValue="Geodwipa Teknika Nusantara" />],
+                        [<div className="flex space-x-2"><p>Jenis penyerahan data</p><p className="text-gray-400">(Submission type)</p></div>, <Input type={"dropdown"} dropdown_items={["Quarterly", "Relinquishment", "Termination", "Spec New", "Spec Ext", "Spec Term", "Joint Study", "DIPA"]} />],
+                        [<div className="flex space-x-2"><p>Nomor AFE</p><p className="text-gray-400">(AFE number)</p></div>, <Input type={"number"} defaultValue='1' />],
+                        [<p className="font-bold">Data type</p>, <Input type={"dropdown"} dropdown_items={["Well data"]} />],
+                        [<p className="font-bold">Data classification</p>, <Input type={"dropdown"} dropdown_items={["Report"]} />],
+                        [<p className="font-bold">Data sub-classification</p>, <Input type={"dropdown"} dropdown_items={["Printed"]} />]
+                    ]
+                } />
+                <div className="h-full">
+                    <TableComponent header={[
+                        <div className="flex justify-between items-center">
+                            <p>Data</p>
+                        </div>
+                    ]} content={[
+                        [<div className="h-[750px]"><Sheets type="review" form_type={router?.query.form_type || "basin"} data={Data} /></div>]
+                    ]} additional_styles_row='p-0' additional_styles="overflow-hidden" />
+                </div>
+                <div className="flex space-x-2 w-full">
+                    <Button path="" button_description="Save document" onClick={(e) => { e.preventDefault(); setIsSaved(true); setMessage("Workspace successfully saved.") }} />
+                    {/* <Button path="" button_description="Unsave document" onClick={(e) => { e.preventDefault(); setIsSaved(false) }} /> */}
+                </div>
+                {/* <p className="bg-black text-white p-2">document saved: {String(IsSaved)}</p> */}
+                <div className={`flex items-center space-x-2 fixed top-5 left-[50%] translate-x-[-50%] bg-green-500 text-white px-3 rounded-lg py-2 transition-all ${Message ? "" : "-translate-y-20"}`}>
+                    <p>{Message}</p>
+                    <Button additional_styles="px-1 py-1 text-black" path="" onClick={(e) => { e.preventDefault(); setMessage("") }}>
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-5 h-5">
+                            <path strokeLinecap="round" strokeLinejoin="round" d="M6 18L18 6M6 6l12 12" />
+                        </svg>
+                    </Button>
+                </div>
+                <div className={`flex items-center space-x-2 fixed top-5 left-[50%] translate-x-[-50%] bg-red-500 text-white px-3 rounded-lg py-2 transition-all ${Error ? "" : "-translate-y-20"}`}>
+                    <p>{Error}</p>
+                    <Button additional_styles="px-1 py-1 text-black" path="" onClick={(e) => { e.preventDefault(); setError("") }}>
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-5 h-5">
+                            <path strokeLinecap="round" strokeLinejoin="round" d="M6 18L18 6M6 6l12 12" />
+                        </svg>
+                    </Button>
+                </div>
+            </Container>
+        ) : (
+            <div className="w-full h-full flex flex-col items-center justify-center space-y-3">
+                <div className="animate-spin border-4 border-t-transparent border-gray-500/[.7] rounded-full w-14 h-14"></div>
+                <p className="text-xl font-semibold text-gray-500">Getting data from database... Please wait</p>
             </div>
-            <div className="flex space-x-2 w-full">
-                <Button path="" button_description="Save document" onClick={(e) => { e.preventDefault(); setIsSaved(true); setMessage("Workspace successfully saved.") }} />
-                {/* <Button path="" button_description="Unsave document" onClick={(e) => { e.preventDefault(); setIsSaved(false) }} /> */}
-            </div>
-            {/* <p className="bg-black text-white p-2">document saved: {String(IsSaved)}</p> */}
-            <div className={`flex items-center space-x-2 fixed top-5 left-[50%] translate-x-[-50%] bg-green-500 text-white px-3 rounded-lg py-2 transition-all ${Message ? "" : "-translate-y-20"}`}>
-                <p>{Message}</p>
-                <Button additional_styles="px-1 py-1 text-black" path="" onClick={(e) => { e.preventDefault(); setMessage("") }}>
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-5 h-5">
-                        <path strokeLinecap="round" strokeLinejoin="round" d="M6 18L18 6M6 6l12 12" />
-                    </svg>
-                </Button>
-            </div>
-            <div className={`flex items-center space-x-2 fixed top-5 left-[50%] translate-x-[-50%] bg-red-500 text-white px-3 rounded-lg py-2 transition-all ${Error ? "" : "-translate-y-20"}`}>
-                <p>{Error}</p>
-                <Button additional_styles="px-1 py-1 text-black" path="" onClick={(e) => { e.preventDefault(); setError("") }}>
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-5 h-5">
-                        <path strokeLinecap="round" strokeLinejoin="round" d="M6 18L18 6M6 6l12 12" />
-                    </svg>
-                </Button>
-            </div>
-        </Container>
+        )
     )
 }
 
