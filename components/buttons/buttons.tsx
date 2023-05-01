ece3c7b104d8f244dd8a46e134a4fed723a304aa
karelbondan <karelbondan@gmail.com>
2023-05-02 00:37:59 +0700

    added additional query parameter for more dynamicity

--- components/buttons/buttons.tsx
+++ components/buttons/buttons.tsx
@@ -4,14 +4,18 @@ import { twMerge } from "tailwind-merge"
 interface ButtonProps extends React.ComponentProps<"button"> {
     path: "";
     title: "";
+    query: "";
     button_description: "";
     additional_styles: "";
 }
 
-const Buttons: React.FunctionComponent<ButtonProps> = ({ path, button_description, title, children = null, additional_styles = '', ...buttonProps }) => {
+const Buttons: React.FunctionComponent<ButtonProps> = ({ path, query, button_description, title, children = null, additional_styles = '', ...buttonProps }) => {
     return (
         <div className={`${buttonProps.disabled ? "pointer-events-none text-black/[.5]" : ""} w-fit select-none`}>
-            <Link href={buttonProps.disabled ? "" : path}>
+            <Link href={{
+                pathname: buttonProps.disabled ? "" : path,
+                query: query
+            }}>
                 <button title={title ? title : button_description} className={twMerge(`flex items-center space-x-2 px-5 py-2 rounded-lg bg-gray-200 hover:bg-gray-300 transition-all`, additional_styles)} {...buttonProps}>
                     {children ? <div>{children}</div> : null}
                     {button_description ? <p>{button_description}</p> : null}
