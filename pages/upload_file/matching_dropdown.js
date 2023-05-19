83bdc56c82feb1a5164905209b3d3919effa121f
karelbondan <karelbondan@gmail.com>
2023-05-20 04:59:36 +0700

    added dynamicity support for other data types, overhauled how the page looks

--- pages/upload_file/matching_dropdown.js
+++ pages/upload_file/matching_dropdown.js
@@ -9,8 +9,13 @@ import HeaderTable, {
   HeaderRow,
   HeaderInput,
 } from "../../components/header_table/header_table";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
+import { setDocumentSummary, setReviewData } from "../../store/generalSlice";
 import { useRouter } from "next/router";
+import { ImageEditor, Tuple4 } from "../components/highlight_viewer";
+import ChevronLeft from '../../public/icons/chevron-left.svg'
+import ChevronRight from '../../public/icons/chevron-right.svg'
+import Highlight from 'react-highlight'
 
 const FullButton = ({ children, onClick }) => {
   return (
@@ -302,7 +307,7 @@ export default function MatchReview({ setTitle }) {
     },
   ];
 
-  const [state, setState] = useState(initialState);
+  const [state, setState] = useState({});
   const [dropDownOptions, setDropDownOptions] = useState([]);
   const [imageBase64Str, setImageBase64Str] = useState("");
   const [docId, _setDocId] = useState(null);
@@ -310,9 +315,15 @@ export default function MatchReview({ setTitle }) {
   const [pageNo, setPageNo] = useState(1);
   const [Loading, setLoading] = useState("")
   const [Message, setMessage] = useState("")
+  const [formType, setformType] = useState("")
+  const [error, setError] = useState("")
+
   const inputFileRef = useRef(null);
+
   const files = useSelector((state) => state.general.file)
   const router = useRouter()
+  const dispatch = useDispatch()
+  const path_query = "Home" + router.pathname.replace(/\//g, " > ").replace(/\_/g, " ")
 
   const setDocId = ((newDocId) => {
     if (docId === null) {
@@ -327,13 +338,19 @@ export default function MatchReview({ setTitle }) {
   });
 
   useEffect(() => {
+    if (router.query.form_type) {
+      setformType(String(router.query.form_type))
+    }
+  }, [router])
+
+  useEffect(() => {
     const onPageChange = async () => {
       if (docId === null) return
       setImageBase64Str(_ => generateImageUrl(docId, pageNo));
       const responseWords = await postScrapeAnnotate(docId, pageNo);
       if (responseWords.status === "success") {
         setDropDownOptions(_ => responseWords.body.words);
-        setState(_ => [...initialState]);
+        // setState(_ => [...initialState]);
       }
     }
     onPageChange();
@@ -353,30 +370,78 @@ export default function MatchReview({ setTitle }) {
         router.push("/upload_file")
         return
       } else {
-        const file = files[0];
-        //   const filename = file.name;
-        //   const size = file.size;
-        const imageBase64Str = await toBase64(file);
-        const uploadResponse = await uploadImage(imageBase64Str);
-        if (uploadResponse.status === "failed") return;
-        const { doc_id: docId } = uploadResponse.body;
-        const summaryResponse = await fetchDocumentSummary(docId);
-        if (summaryResponse.status === "success") {
+        try {
+          const file = files[0];
+          //   const filename = file.name;
+          //   const size = file.size;
+          const imageBase64Str = await toBase64(file);
+
+          setLoading("Uploading document...")
+          const uploadResponse = await uploadImage(imageBase64Str);
+          if (uploadResponse.status === "failed" || !uploadResponse.body) {
+            throw "Something went wrong with the OCR service. Response body returned null on file upload."
+          };
+          const { doc_id: docId } = uploadResponse.body;
+
+          setLoading("Getting uploaded document's summary...")
+          const summaryResponse = await fetchDocumentSummary(docId);
+          if (summaryResponse.status !== "success" || !summaryResponse.body.page_count) {
+            throw "Something went wrong with the OCR service. Response body returned null on document summary."
+          }
           setTotalPageNo(_ => summaryResponse.body.page_count);
-        }
-        setDocId(docId);
-        const result = await postScrapeAnnotate(docId, pageNo);
-        if (result.status === "success") {
+          dispatch(setDocumentSummary({ ...summaryResponse, document_id: docId }))
+          setDocId(docId);
+
+          setLoading("Populating drop down items...")
+          const result = await postScrapeAnnotate(docId, pageNo);
+          if (result.status !== "success" || !result.body.words) {
+            throw "Something went wrong with the OCR service. Response body returned null on word scraping."
+          }
           setImageBase64Str((_) => generateImageUrl(docId, pageNo));
           setDropDownOptions((_) => result.body.words);
-          setState((_) => initialState);
+          // setState((_) => initialState);
+
+          setLoading(`Getting appropriate properties for data type ${router.query.form_type}`)
+          const row_names = await fetch('http://localhost:5050/getHeaders', {
+            method: "POST",
+            headers: {
+              "Content-Type": "application/json"
+            },
+            // TODO change form_type to be dynamic later
+            // FINISHED
+            body: JSON.stringify({
+              form_type: router.query?.form_type || "basin"
+            })
+          }).then(response => {
+            return response.json()
+          }).catch(error => { throw error })
+
+          setLoading(`Setting appropriate properties for data type ${router.query.form_type}`)
+          let temp_obj = {}
+          for (let idx = 0; idx < summaryResponse.body.page_count; idx++) {
+            let temp = []
+            row_names.response.forEach((row_name, index) => {
+              temp.push({
+                id: index,
+                key: row_name.toLowerCase(),
+                value: "",
+              },)
+            });
+            temp_obj[idx] = temp
+          }
+          setState(temp_obj)
+          setLoading("");
+        } catch (error) {
+          setError(String(error))
         }
       }
       router.events.emit("routeChangeComplete")
       setLoading("")
+      setTimeout(() => {
+        setMessage("Make sure you have inputted all of the data correctly before proceeding to view them in the spreadsheet.")
+      }, 3000)
     }
     init()
-    setMessage("Make sure you have inputted all of the data correctly before proceeding to the next step (viewing the data in spreadsheet form).")
   }, [files])
 
   useEffect(() => {
@@ -401,7 +466,7 @@ export default function MatchReview({ setTitle }) {
       if (result.status === "success") {
         setImageBase64Str((_) => result.body.base64str);
         setDropDownOptions((_) => result.body.dropdown);
-        setState((_) => initialState);
+        // setState((_) => initialState);
       }
     } else {
     }
@@ -418,16 +483,28 @@ export default function MatchReview({ setTitle }) {
 
   const setValueForId = (id, value) => {
     setState((state) => {
-      const index = state.findIndex((pair) => pair.id === id);
-      const cpair = state.find((pair) => pair.id === id);
+      // creating a copy to prevent direct mutation that causes error in redux
+      let final = { ...state }
+      const index = final[pageNo - 1].findIndex((pair) => pair.id === id);
+      const cpair = final[pageNo - 1].find((pair) => pair.id === id);
       const newPair = { ...cpair, value };
-      return [...state.slice(0, index), newPair, ...state.slice(index + 1)];
+      final[pageNo - 1] = [...final[pageNo - 1].slice(0, index), newPair, ...final[pageNo - 1].slice(index + 1)]
+      console.log(final)
+      return { ...final };
+
+      // const index = state.findIndex((pair) => pair.id === id);
+      // const cpair = state.find((pair) => pair.id === id);
+      // const newPair = { ...cpair, value };
+      // return [...state.slice(0, index), newPair, ...state.slice(index + 1)];
     });
   };
 
+  // TODO CHANGE THIS TO USE REDUX
   useEffect(() => {
-    localStorage.setItem('reviewData', JSON.stringify(state))
-    console.log(localStorage.getItem('reviewData'))
+    console.log(state)
+    dispatch(setReviewData(state))
+    // localStorage.setItem('reviewData', JSON.stringify(state))
+    // console.log(localStorage.getItem('reviewData'))
   }, [state])
 
 
@@ -448,7 +525,7 @@ export default function MatchReview({ setTitle }) {
   const toRowComponent = (data) => (
     <div key={data.id}>
       <HeaderDivider />
-      <HeaderInput label1={data.key}>
+      <HeaderInput label1={data.key.replace(/\_/g, " ").split(' ').map((s) => s.charAt(0).toUpperCase() + s.substring(1)).join(' ')}>
         <Input
           value={data.value}
           type="dropdown"
@@ -497,25 +574,41 @@ export default function MatchReview({ setTitle }) {
   );
 
   // TODO FIX THIS WORKFLOW TO INCLUDE MULTIPLE PAGES AND INTERACTIVE DOCUMENT VIEWER
-  
+  // DONE
+
   return (
-    Loading ?
+    (Loading) ? (
       <div className="w-full h-full flex flex-col items-center justify-center space-y-3">
         <div className="animate-spin border-4 border-t-transparent border-gray-500/[.7] rounded-full w-14 h-14"></div>
         <p className="text-xl font-semibold text-gray-500">{Loading}</p>
       </div>
-      :
+    ) : (error) ? (
+      <div className="w-full h-full flex flex-col p-10 space-y-4">
+        <p className="font-bold text-lg text-red-500">Something happened. Please try again or contact administrator/maintainer if the problem still persists by giving them the information below:</p>
+        <Highlight className='html rounded-md border-2'>{error}</Highlight>
+        {/* @ts-ignore */}
+        <Buttons path="" button_description="Back" onClick={() => { router.back() }} />
+      </div>
+    ) : (
       <Container additional_class="full-height relative">
-        <Container.Title>Data Matching</Container.Title>
-        <div className="grid grid-cols-2">
+        <Container.Title>
+          <div className="-space-y-2">
+            <p className="capitalize text-sm font-normal">{path_query}</p>
+            <p>Data Matching</p>
+          </div>
+        </Container.Title>
+        <div className="grid grid-cols-2 gap-2 border-[2px] rounded-lg p-2">
           <HeaderTable>
-            {state.map(toRowComponent)}
-            <HeaderDivider />
+            {state[pageNo - 1]?.map(toRowComponent)}
+            {/* <HeaderDivider /> */}
             {/* <HeaderRow>
             <FullButton onClick={addPair}>+</FullButton>
           </HeaderRow> */}
           </HeaderTable>
-          <img src={imageBase64Str} alt="" className="object-contain m-auto" />
+          <div className="h-[calc(100vh-55px)] sticky top-0 grid grid-cols-1 rounded-lg overflow-clip">
+            <ImageEditor boundsObserver={() => { }} imageUrl={imageBase64Str} />
+          </div>
+          {/* <img src={imageBase64Str} alt="" className="object-contain m-auto" /> */}
         </div>
         <input
           accept="image/*"
@@ -525,6 +618,22 @@ export default function MatchReview({ setTitle }) {
           hidden
           onChange={handleChangeUploadFile}
         />
+        {(totalPageNo > 1) ? (
+          <div className="flex items-center justify-center sticky bottom-2 my-4 z-[10000] w-full pointer-events-none">
+            <div className="w-fit flex space-x-2 items-center justify-center bg-white rounded-lg p-2 border pointer-events-auto">
+              {/* @ts-ignore */}
+              <Buttons path="" title="Previous page" button_description="" additional_styles="bg-white border-2 p-3 hover:bg-gray-200" onClick={prevPage} disabled={pageNo > 1 ? false : true} ><div className="w-5 h-5"><ChevronLeft /></div></Buttons>
+              {/* @ts-ignore */}
+              <div path="" title="" button_description="" className="bg-white border-2 p-3 cursor-default select-none text-center rounded-lg"><p className="w-5 h-5">{pageNo}</p></div>
+              {/* @ts-ignore */}
+              <Buttons path="" title="Next page" button_description="" additional_styles="bg-white border-2 p-3 hover:bg-gray-200" onClick={nextPage} disabled={pageNo >= totalPageNo ? true : false}><div className="w-5 h-5"><ChevronRight /></div></Buttons>
+            </div>
+          </div>
+        ) : null}
+        <div className="flex items-center justify-center w-full py-4">
+          {/* @ts-ignore */}
+          <Buttons button_description="View on sheets" path="/upload_file/review" query={{ form_type: formType }} additional_styles="px-20 bg-searchbg/[.6] hover:bg-searchbg font-semibold" disabled={formType ? false : true} onClick={() => { dispatch(setReviewData(state)) }} />
+        </div>
         <ButtonsSection>
           {/* <Buttons
             path=""
@@ -543,9 +652,9 @@ export default function MatchReview({ setTitle }) {
           <Buttons path="" additional_styles="text-error">
             Cancel
           </Buttons> */}
-          <Buttons button_description="View on sheets" path="/upload_file/review" additional_styles="bg-primary" />
+          {/* <Buttons button_description="View on sheets" path="/upload_file/review" additional_styles="bg-primary" />
           <Buttons path="" additional_styles="bg-primary" button_description="Previous Page" onClick={prevPage} />
-          <Buttons path="" additional_styles="bg-primary" button_description="Next Page" onClick={nextPage} />
+          <Buttons path="" additional_styles="bg-primary" button_description="Next Page" onClick={nextPage} /> */}
         </ButtonsSection>
         <div className={`flex items-center space-x-2 fixed top-5 left-[50%] translate-x-[-50%] bg-green-500 text-white px-3 rounded-lg py-2 transition-all ${Message ? "" : "-translate-y-20"}`}>
           <p>{Message}</p>
@@ -556,5 +665,6 @@ export default function MatchReview({ setTitle }) {
           </Buttons>
         </div>
       </Container>
+    )
   );
 }
