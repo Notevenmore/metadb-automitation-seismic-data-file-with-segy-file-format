dedf9532e6f7c737c67a4d5826e78641a9db4834
karelbondan <karel.herdito@binus.ac.id>
2023-06-13 22:10:54 +0700

    successfully connected to the backend endpoint APIs; adjustment for new workflow

--- pages/upload_file/index.js
+++ pages/upload_file/index.js
@@ -5,56 +5,9 @@ import { useDispatch } from "react-redux";
 import Buttons from "../../components/buttons/buttons";
 import Container from "../../components/container/container.js";
 import Input from "../../components/input_form/input";
-import { storeFile } from "../../store/generalSlice";
+import { storeFile, setUploadDocumentSettings } from "../../store/generalSlice";
 import Select from "../../public/icons/selection_tool.svg"
-
-const datatypes = {
-	"Basin": "basin",
-	"Working Area": "working_area",
-	"Field Information ": "field_information",
-	"2D Seismic Summary": "2d_seismic_summary",
-	"2D Seismic Section": "2d_seismic_section",
-	"Digital 2D Seismic Section": "digital_2d_seismic_section",
-	"2D Seismic Field Data Stored In Media": "2d_seismic_field_data_stored_in_media",
-	"2D Seismic Process Data Stored In Media": "2d_seismic_process_data_stored_in_media",
-	"2D Seismic Field Digital Data": "2d_seismic_field_digital_data",
-	"2D Seismic Processed Digital Data": "2d_seismic_processed_digital_data",
-	"2D Seismic Navigation Digital Data": "2d_seismic_navigation_digital_data",
-	"2D Seismic Navigation Data Stored In Media": "2d_seismic_navigation_data_stored_in_media",
-	"3D Seismic Summary": "3d_seismic_summary",
-	"3D Seismic Field Data Stored In Media": "3d_seismic_field_data_stored_in_media",
-	"3D Seismic Process Data Stored In Media": "3d_seismic_process_data_stored_in_media",
-	"3D Seismic Field Digital Data": "3d_seismic_field_digital_data",
-	"3D Seismic Processed Digital Data": "3d_seismic_processed_digital_data",
-	"3D Seismic Navigation Digital Data": "3d_seismic_navigation_digital_data",
-	"3D Seismic Navigation Data Stored In Media": "3d_seismic_navigation_data_stored_in_media",
-	"2D/3D Seismic Printer Report": "2d_3d_seismic_printer_report",
-	"2D/3D Seismic Digital Report": "2d_3d_seismic_digital_report",
-	"Seismic Interpretation Data": "seismic_interpretation_data",
-	"Non-Seismic And Seismic Unconventional Survey Summary": "non-seismic_and_seismic_unconventional_survey_summary",
-	"Non-Seismic And Seismic Unconventional Data Stored In Media": "non-seismic_and_seismic_unconventional_data_stored_in_media",
-	"Non-Seismic And Seismic Unconventional Digital Data": "non-seismic_and_seismic_unconventional_digital_data",
-	"Non-Seismic And Seismic Unconventional Printed Report": "non-seismic_and_seismic_unconventional_printed_report",
-	"Non-Seismic And Seismic Unconventional Digital Report": "non-seismic_and_seismic_unconventional_digital_report",
-	"Well Summary": "well_summary",
-	"Printed Well Log": "printed_well_log",
-	"Digital Image Well Log": "digital_image_well_log",
-	"Digital Well Log": "digital_well_log",
-	"Printed Well Report": "printed_well_report",
-	"Digital Well Report": "digital_well_report",
-	"Digital Well Seismic Profile": "digital_well_seismic_profile",
-	"Well Seismic Profile Stored In Media": "well_seismic_profile_stored_in_media",
-	"Well Sample": "well_sample",
-	"Well Core Sample": "well_core_sample",
-	"Outcrop Sample": "outcrop_sample",
-	"Printed Technical Report": "printed_technical_report",
-	"Digital Technical Report": "digital_technical_report",
-	"Printed Maps And Technical Drawing": "printed_maps_and_technical_drawing",
-	"Digital Maps And Technical Drawing": "digital_maps_and_technical_drawing",
-	"Digital Project File": "digital_project_file",
-	"Project File Data Stored In Media": "project_file_data_stored_in_media",
-	"Bibliography": "bibliography"
-}
+import config, { datatypes } from "../config";
 
 export default function UploadFilePage({ setTitle }) {
 	setTitle("Upload file")
@@ -62,21 +15,19 @@ export default function UploadFilePage({ setTitle }) {
 	const path_query = "Home" + router.pathname.replace(/\//g, " > ").replace(/\_/g, " ")
 	const additional_styles_label = "w-[20%] font-semibold";
 	const [fileUpload, setFileUpload] = useState([]);
-	const [previousSelectedMethod, setpreviousSelectedMethod] = useState()
-	// const [DataType, setDataType] = useState()
-	// const [DataClassification, setDataClassification] = useState()
-	// const [DataSubClass, setDataSubClass] = useState()
-	// const [FileFormat, setFileFormat] = useState()
 	const [Error, setError] = useState("")
 	const [UplSettings, setUplSettings] = useState({
+		workspace_name: "",
+		kkks_name: "",
+		working_area: "",
+		submission_type: "",
+		afe_number: "",
 		DataType: "",
-		// DataClassification: "",
-		// DataSubClass: "",
 		FileFormat: "",
 		Method: ""
 	})
-
-	// test commit
+	const [toggleOverlay, settoggleOverlay] = useState(false)
+	const [Message, setMessage] = useState({ message: "", color: "" })
 
 	const fileUploadRef = useRef(null);
 
@@ -112,17 +63,64 @@ export default function UploadFilePage({ setTitle }) {
 	const handleSubmit = () => {
 		// e.preventDefault()
 		// if (fileUpload.length < 1 || !UplSettings.DataType || !UplSettings.DataClassification || !UplSettings.DataSubClass || !UplSettings.FileFormat ) {
-		if (fileUpload.length < 1 || !UplSettings.DataType || !UplSettings.FileFormat) {
-			setError("Please select a file before continuing to the next process. Make sure to also fill in the appropriate settings for the uploaded file.")
-			return false
-		}
+		// if (fileUpload.length < 1 || Object.values(UplSettings).some(x => { return x === null || x === "" })) {
+		// 	setError("Please select a file before continuing to the next process. Make sure to also fill in the appropriate settings for the uploaded file.")
+		// 	return false
+		// }
 		dispatch(storeFile(fileUpload))
+		dispatch(setUploadDocumentSettings(UplSettings))
+		settoggleOverlay(true)
 		// router.push('/upload_file/matching_guided');
 		// router.push('/upload_file/matching');
 		// router.push('/upload_file/matching_auto');
 		// router.push('/upload_file/matching_draggable');
 		// console.log("???")
 		return true
+	}
+
+	const delay = delay_amount_ms =>
+		new Promise(resolve => setTimeout(() => resolve("delay"), delay_amount_ms))
+
+	const proceed = async (e, submit = false) => {
+		e.preventDefault()
+		settoggleOverlay(false)
+		if (submit) {
+			setMessage({ message: "Creating a new workspace... Please don't leave this page or click anything", color: "blue" });
+			if (fileUpload.length < 1 || Object.values(UplSettings).some(x => { return x === null || x === "" })) {
+				setError("Please select a file before continuing to the next process. Make sure to also fill in the appropriate settings for the uploaded file.")
+				return false
+			}
+			const post_workspace = await fetch(`${config[router.query.form_type]["afe"]}`, {
+				method: "POST",
+				headers: {
+					"Content-Type": "application/json"
+				},
+				body: JSON.stringify({
+					"afe_number": parseInt(UplSettings.afe_number),
+                    "workspace_name": UplSettings.workspace_name,
+                    "kkks_name": UplSettings.kkks_name,
+                    "working_area": UplSettings.working_area,
+                    "submission_type": UplSettings.submission_type
+				})
+			}).then(res => {
+				if (res.status !== 200) {
+					throw "Failed to POST new workspace. Please try again."
+				}
+				return res.text()
+			})
+			if (post_workspace === "OK") {
+				setMessage({ message: "Success. A new workspace has been created. Redirecting to the next page...", color: "blue" });
+				await delay(1000)
+				router.push({
+					pathname: UplSettings.Method === "dropdown" ? '/upload_file/matching_dropdown' : UplSettings.Method === "highlight" ? '/upload_file/matching_highlight' : UplSettings.Method === "dragdrop" ? '/upload_file/matching_draggable' : '/upload_file/matching_auto',
+					query: {
+						form_type: datatypes[UplSettings.DataType]
+					}
+				})
+			} else {
+				setMessage({ message: "Failed to create a new workspace. Please try again or contact maintainer if the problem persists.", color: "red" });
+			}
+		}
 	}
 
 	useEffect(() => {
@@ -172,14 +170,84 @@ export default function UploadFilePage({ setTitle }) {
 					</div>
 				</div>
 				{/* <div className="border border-b-[#7a7a7a] w-full lg:w-[95%]"></div> */}
-				<h2 className="text-xl font-bold py-3">Choose the right settings for the uploaded file</h2>
+				<h2 className="text-xl font-bold py-3">Input the correct settings for the uploaded file and the new workspace</h2>
 				<div className="w-[100%] lg:w-[80%] flex flex-col gap-y-3">
+					<Input
+						label="Workspace name"
+						label_loc="beside"
+						type="text"
+						name={"workingArea"}
+						placeholder={"Workspace name"}
+						value={UplSettings.workspace_name}
+						required={true}
+						additional_styles="w-full"
+						additional_styles_label={additional_styles_label}
+						autoComplete="off"
+						onChange={(e) => setUplSettings({ ...UplSettings, workspace_name: e.target.value })}
+					/>
+					<Input
+						label="AFE number"
+						label_loc="beside"
+						type="number"
+						name={"AFE_Number"}
+						placeholder={"1945"}
+						value={UplSettings.afe_number}
+						required={true}
+						additional_styles="w-full"
+						additional_styles_label={additional_styles_label}
+						autoComplete="off"
+						onChange={(e) => setUplSettings({ ...UplSettings, afe_number: e.target.value })}
+					/>
+					<Input
+						label="KKKS name"
+						label_loc="beside"
+						type="text"
+						name={"kkksName"}
+						placeholder={"Geodwipa Teknika Nusantara"}
+						value={UplSettings.kkks_name}
+						required={true}
+						additional_styles="w-full"
+						additional_styles_label={additional_styles_label}
+						autoComplete="off"
+						onChange={(e) => setUplSettings({ ...UplSettings, kkks_name: e.target.value })}
+					/>
+					<Input
+						label="Working area"
+						label_loc="beside"
+						type="text"
+						name={"workingArea"}
+						placeholder={"Pulau Geodwipa"}
+						value={UplSettings.working_area}
+						required={true}
+						additional_styles="w-full"
+						additional_styles_label={additional_styles_label}
+						autoComplete="off"
+						onChange={(e) => setUplSettings({ ...UplSettings, working_area: e.target.value })}
+					/>
+					<Input
+						label="Submission type"
+						label_loc="beside"
+						type="dropdown"
+						name={"submissionType"}
+						placeholder={"Select a submission type"}
+						value={UplSettings.submission_type}
+						dropdown_items={[
+							"Quarterly", "Relinquishment", "Termination", "Spec New", "Spec Ext", "Spec Term", "Joint Study", "DIPA"
+						]}
+						required={true}
+						additional_styles="w-full"
+						additional_styles_input="placeholder: italic"
+						additional_styles_label={additional_styles_label}
+						onChange={(e) => setUplSettings({ ...UplSettings, submission_type: e.target.value })}
+						withSearch
+					/>
 					<Input
 						label="Data type"
 						label_loc="beside"
 						type="dropdown"
 						name={"dataType"}
-						placeholder={"Data type"}
+						placeholder={"Select a data type"}
+						value={UplSettings.DataType}
 						dropdown_items={Object.keys(datatypes)}
 						required={true}
 						additional_styles="w-full"
@@ -187,38 +255,13 @@ export default function UploadFilePage({ setTitle }) {
 						onChange={(e) => setUplSettings({ ...UplSettings, DataType: e.target.value })}
 						withSearch
 					/>
-					{/* <Input
-						label="Data classification"
-						label_loc="beside"
-						type="dropdown"
-						name={"dataClassification"}
-						placeholder={"Data classification"}
-						dropdown_items={["Report"]}
-						required={true}
-						additional_styles="w-full"
-						additional_styles_label={additional_styles_label}
-						onChange={(e) => setUplSettings({ ...UplSettings, DataClassification: e.target.value })}
-						withSearch
-					/>
-					<Input
-						label="Sub data classification"
-						label_loc="beside"
-						type="dropdown"
-						name={"subDataClassification"}
-						placeholder={"Sub data classification"}
-						dropdown_items={["Printed"]}
-						required={true}
-						additional_styles="w-full"
-						additional_styles_label={additional_styles_label}
-						onChange={(e) => setUplSettings({ ...UplSettings, DataSubClass: e.target.value })}
-						withSearch
-					/> */}
 					<Input
 						label="File format"
 						label_loc="beside"
 						type="dropdown"
 						name={"fileFormat"}
-						placeholder={"File format"}
+						placeholder={"Select a file format"}
+						value={UplSettings.FileFormat}
 						dropdown_items={["Image", "PDF", "PPTX", "CSV", "LAS", "SEGY"]}
 						required={true}
 						additional_styles="w-full"
@@ -243,7 +286,7 @@ export default function UploadFilePage({ setTitle }) {
 						</Buttons>
 						<Buttons id="highlight" title="" additional_styles={`h-full active:bg-gray-400/60 outline-none ${UplSettings.Method === "highlight" ? "bg-searchbg/60" : ""}`} onClick={(e) => { setUplSettings({ ...UplSettings, Method: "highlight" }) }}>
 							<div className="flex space-x-2 min-w-max items-center p-2">
-								<Select className="w-10 h-10"/>
+								<Select className="w-10 h-10" />
 								<section className="w-[150px]">
 									<h3 className="text-lg font-bold">Highlighting</h3>
 									<p className="text-sm">Match the data by highlighting the document preview.</p>
@@ -275,30 +318,71 @@ export default function UploadFilePage({ setTitle }) {
 				<div className="flex flex-row gap-x-3 pt-3 pb-16">
 					<Buttons
 						type="submit"
-						path={(fileUpload.length >= 1 && UplSettings.DataType && UplSettings.FileFormat && UplSettings.Method) ? UplSettings.Method === "dropdown" ? '/upload_file/matching_dropdown' : UplSettings.Method === "highlight" ? '/upload_file/matching_highlight' : UplSettings.Method === "dragdrop" ? '/upload_file/matching_draggable' : '/upload_file/matching_auto' : ''}
+						path={(fileUpload.length <= 1 && Object.values(UplSettings).some(x => { return x === null || x === "" })) ? UplSettings.Method === "dropdown" ? '/upload_file/matching_dropdown' : UplSettings.Method === "highlight" ? '/upload_file/matching_highlight' : UplSettings.Method === "dragdrop" ? '/upload_file/matching_draggable' : '/upload_file/matching_auto' : ''}
 						query={
 							{ form_type: datatypes[UplSettings.DataType] }
 						}
 						button_description="Upload and process file"
 						additional_styles="bg-searchbg/[.6] hover:bg-searchbg font-semibold"
 						onClick={handleSubmit}
-						disabled={(fileUpload.length >= 1 && UplSettings.DataType && UplSettings.FileFormat && UplSettings.Method) ? false : true}
+						disabled={(fileUpload.length <= 1 && Object.values(UplSettings).some(x => { return x === null || x === "" })) ? true : false}
 					/>
 					<Buttons type="submit" button_description="Cancel" path="" onClick={router.back} />
 				</div>
 			</form>
+			<div className={`fixed w-screen h-screen flex items-center justify-center bg-black/[.5] top-0 left-0 ${toggleOverlay ? "opacity-100 visible" : "opacity-0 invisible"} transition-all`}>
+				<div className="flex items-center justify-center w-[50%] h-full">
+					<div className={`bg-white w-fit h-fit border-2 rounded-lg p-10 relative space-y-3 ${toggleOverlay ? "" : "-translate-y-10 opacity-0"} transition-all`}>
+						<Buttons path="" additional_styles="absolute top-2 right-2 px-1 py-1 text-black" title="Cancel" onClick={(e) => { e.preventDefault(); settoggleOverlay(false); setnewWorkspace({ workspace_name: "", kkks_name: "", working_area: "", afe_number: "", submission_type: "" }) }}>
+							<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-5 h-5">
+								<path strokeLinecap="round" strokeLinejoin="round" d="M6 18L18 6M6 6l12 12" />
+							</svg>
+						</Buttons>
+						<h1 className="font-bold text-3xl">Re-check the inputted data</h1>
+						<hr />
+						<p>Do you want to proceed? Make sure you have re-checked all the inputs especially the ones for the new workspace. <strong>You won't be able to change AFE number and Data Type after the workspace has been created.</strong></p>
+						<section className="flex w-full items-center justify-center space-x-2">
+							<Buttons onClick={(e) => { proceed(e, true) }} additional_styles="bg-searchbg/[.6] hover:bg-searchbg font-semibold">Proceed</Buttons>
+							<Buttons onClick={proceed}>Cancel</Buttons>
+						</section>
+					</div>
+				</div>
+			</div>
 			{dragActive && (
 				<div
-					className="absolute w-full h-full top-0 right-0 z-[50] bg-primary/[20%]"
+					className="absolute w-full full-height top-0 right-0 z-[50] bg-searchbg/[20%] flex items-center justify-center backdrop-blur-md"
 					onDragEnter={(e) => handleDrag(e)}
 					onDragLeave={(e) => handleDrag(e)}
 					onDragOver={(e) => handleDrag(e)}
-					onDrop={(e) => handleDrop(e)}></div>
+					onDrop={(e) => handleDrop(e)}>
+					{/* <p className="text-4xl font-bold">Drop your file here</p> */}
+					<div className="border-dashed border-4 border-black/30 rounded-lg p-10 flex flex-col justify-center space-y-3 items-center w-[80%] bg-searchbg/[.5] h-[550px]">
+						<div className="w-full text-center">
+							<p className="text-4xl font-bold">Drop your file here</p>
+						</div>
+						<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-16 h-16">
+							<path strokeLinecap="round" strokeLinejoin="round" d="M20.25 7.5l-.625 10.632a2.25 2.25 0 01-2.247 2.118H6.622a2.25 2.25 0 01-2.247-2.118L3.75 7.5m8.25 3v6.75m0 0l-3-3m3 3l3-3M3.375 7.5h17.25c.621 0 1.125-.504 1.125-1.125v-1.5c0-.621-.504-1.125-1.125-1.125H3.375c-.621 0-1.125.504-1.125 1.125v1.5c0 .621.504 1.125 1.125 1.125z" />
+						</svg>
+					</div>
+				</div>
 			)}
 			<div className={`flex items-center space-x-2 fixed top-5 left-[50%] translate-x-[-50%] bg-red-500 text-white px-3 rounded-lg py-2 transition-all ${Error ? "" : "-translate-y-20"}`}>
 				<p>{Error}</p>
 				<Buttons additional_styles="px-1 py-1 text-black" path="" onClick={() => { setError("") }}>
 					<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-5 h-5">
+						<path strokeLinecap="round" strokeLinejoin="round" d="M6 18L18 6M6 6l12 12" />
+					</svg>
+				</Buttons>
+			</div>
+			<div className={`flex items-center space-x-2 fixed top-5 left-[50%]
+                 translate-x-[-50%] bg-${Message.color || "blue"}-500 text-white
+                 px-3 rounded-lg py-2 transition-all ${Message.message ? "" : "-translate-y-20"}`}>
+				<p>{Message.message}</p>
+				<Buttons
+					additional_styles="px-1 py-1 text-black" path=""
+					onClick={(e) => { e.preventDefault(); setMessage({ message: "", color: "" }) }}>
+					<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+						strokeWidth={1.5} stroke="currentColor" className="w-5 h-5">
 						<path strokeLinecap="round" strokeLinejoin="round" d="M6 18L18 6M6 6l12 12" />
 					</svg>
 				</Buttons>
