618e426012c3d79f6e880ae88b56ab672986b222
karelbondan <karel.herdito@binus.ac.id>
2023-06-16 23:07:41 +0700

    fix server and client desync problem; added animation; fixed overflow problem

--- components/navigation_bar/SideBar.js
+++ components/navigation_bar/SideBar.js
@@ -7,36 +7,39 @@ export default function SideBar({ half }) {
 	const [iconCollapse, setIconCollapse] = useState(false);
 	return (
 		<div
-			className={`float-left ${
-				half ? "h-full" : "h-screen"
-			} flex flex-col pt-2 pb-10 w-[314px] bg-side_bar overflow-y-auto relative`}
-			style={{ width: iconCollapse ? "auto" : "" }}>
-			{iconCollapse ? (
-				<div>
-					<Item icon="/icons/magnify.svg" collapse={iconCollapse} />
-                    <div className="border-b border-b-[#dddddd]"></div>
+			// className={`float-left ${half ? "h-full" : "h-screen"} ${iconCollapse ? "w-auto" : ""} flex flex-col pt-2 pb-10 w-[314px] bg-side_bar overflow-y-auto relative`}
+			className={`float-left justify-between full-height ${iconCollapse ? "w-[57px]" : "w-[314px]"} flex flex-col bg-side_bar overflow-y-auto overflow-x-hidden relative transition-all`}
+		// style={{ width: iconCollapse ? "auto" : "" }}>
+		>
+			<div className="overflow-y-auto pb-10 overflow-x-hidden">
+				{iconCollapse ? (
+					<div>
+						<Item icon="/icons/magnify.svg" collapse={iconCollapse} />
+						<div className="border-b border-b-[#dddddd]"></div>
+					</div>
+				) : (
+					<div>
+						<SearchWidget />
+					</div>
+				)}
+				<div onClick={() => setIconCollapse((prev) => !prev)}>
+					<Item
+						name="Collapse"
+						icon={iconCollapse ? "/icons/chevron-double-right.svg" : "/icons/chevron-double-left.svg"}
+						collapse={iconCollapse}></Item>
 				</div>
-			) : (
-				<div>
-					<SearchWidget />
-				</div>
-			)}
-			<div onClick={() => setIconCollapse((prev) => !prev)}>
-				<Item
-					name="Collapse"
-					icon={iconCollapse ? "/icons/chevron-double-right.svg" : "/icons/chevron-double-left.svg"}
-					collapse={iconCollapse}></Item>
+				{List.map((router) => (
+					<Item
+						name={router.name}
+						icon={router.icon}
+						child={router.child}
+						link={router.link}
+						collapse={iconCollapse}
+						setCollapse={setIconCollapse}
+						key={router.name}></Item>
+				))}
 			</div>
-			{List.map((router) => (
-				<Item
-					name={router.name}
-					icon={router.icon}
-					child={router.child}
-					link={router.link}
-					collapse={iconCollapse}
-					key={router.name}></Item>
-			))}
-			<div className={`fixed bottom-0 h-[40px] ${iconCollapse? "hidden" :"w-[314px]"} flex items-center justify-center text-[12px] text-[#a3a3a3] bg-side_bar`}>
+			<div className={`min-h-[35px] ${iconCollapse ? "hidden" : "w-[314px]"} flex items-center justify-center text-[12px] text-[#a3a3a3] bg-side_bar`}>
 				<div>&copy; PT Geodwipa Teknika Nusantara</div>
 			</div>
 		</div>
