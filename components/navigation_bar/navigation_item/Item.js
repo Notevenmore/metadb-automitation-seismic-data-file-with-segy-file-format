ee18073a33d0c04ecf7d6bffff7156d4aef806c7
karelbondan <karel.herdito@binus.ac.id>
2023-06-16 23:09:42 +0700

    added animation; added highlight according to current path

--- components/navigation_bar/navigation_item/Item.js
+++ components/navigation_bar/navigation_item/Item.js
@@ -2,18 +2,28 @@ import Icon from "@mdi/react";
 import { mdiChevronRight } from "@mdi/js";
 import Link from "next/link";
 import styles from "../../../styles/NavItem.module.css";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { useRouter } from "next/router";
 
-export default function Item({ icon, name, child, link, collapse }) {
+export default function Item({ icon, name, child, link, collapse, setCollapse }) {
     const [toggleOpen, setToggleOpen] = useState(false);
+    const [selected, setselected] = useState("")
     function toggle() {
-        setToggleOpen(prevToggle => !prevToggle)
+        if (collapse) {
+            setToggleOpen(true)
+        } else {
+            setToggleOpen(prevToggle => !prevToggle)
+        }
     }
+
+    const router = useRouter()
+
+
+
 
     return (
         <div className="text-[14.5px]">
-            <div onClick={toggle}>
+            <div onClick={() => { toggle(); try { setCollapse(false) } catch (error) { } }}>
                 <Child
                     icon={icon}
                     name={name}
@@ -24,13 +34,17 @@ export default function Item({ icon, name, child, link, collapse }) {
             {!collapse && child && toggleOpen && (
                 <div className="ml-4">
                     {child.map((item, index) => (
-                        <Child
-                            key={index}
-                            icon={item.icon}
-                            name={item.name}
-                            link={item.link}
-                            collapse={collapse}
-                        ></Child>
+                        <div onClick={() => { setselected(item.name) }}>
+                            <Child
+                                key={index}
+                                icon={item.icon}
+                                name={item.name}
+                                link={item.link}
+                                collapse={collapse}
+                                selected={selected}
+                                setSelected={setselected}
+                            ></Child>
+                        </div>
                     ))}
                 </div>
             )}
@@ -38,17 +52,29 @@ export default function Item({ icon, name, child, link, collapse }) {
     );
 }
 
-function Child({ icon, name, link="", collapse }) {
+function Child({ icon, name, link = "", collapse }) {
+    const [currentPath, setcurrentPath] = useState("")
     const router = useRouter()
+
+    // useEffect(() => {
+    //     setcurrentPath(router.asPath)
+    //     // setcurrentPath(router.asPath.split("/").join(" ").split("_").map(word => { return word.charAt(0).toUpperCase() + word.slice(1) }).join(" "))
+    //     // console.log(/\/\w+$/g)
+    //     // console.log(router.asPath.match(/\/\w+$/g))
+    //     // console.log(router.asPath.split("/"))
+    //     // console.log(router.asPath.split("/").some(path => {return path === name.toLowerCase().replace(/\s/g, "_")}))
+    //     // console.log(router.asPath, name.toLowerCase().replace(/\s/g, "_"))
+    // }, [router.asPath])
+
     return (
         <Link href={link || router.asPath} className={styles.navItem}>
-            <div className="flex justify-between items-center px-5 py-2 gap-x-4 hover:bg-gray-200">
+            <div className={`flex justify-between items-center px-5 py-2 gap-x-4 hover:bg-gray-200 ${router.asPath.split("/").some(path => {return path === name?.toLowerCase().replace(/\s/g, "_")}) ? "bg-[#dae0e5]" : ""} relative transition-all`}>
                 <div className="flex gap-x-4">
                     <img src={icon} className="w-[.9rem] h-[22px]" alt="icon" />
-                    {!collapse && <div>{name}</div>}
+                    {!collapse && <div className="w-[200px]">{name}</div>}
                 </div>
                 {!collapse && (
-                    <div>
+                    <div className="absolute right-5">
                         <img src="/icons/chevron-right.svg" alt="icon" className={`w-[25px] h-[15px] ${styles.navItemChevron}`} />
                     </div>
                 )}
