d718090d8a87d51a1c966b19b03272bc440914a4
karelbondan <karel.herdito@binus.ac.id>
2023-06-16 23:06:11 +0700

    exclude Link component if path is empty to avoid interpolation error

--- components/buttons/buttons.tsx
+++ components/buttons/buttons.tsx
@@ -7,10 +7,11 @@ interface ButtonProps extends React.ComponentProps<"button"> {
     query: "";
     button_description: "";
     additional_styles: "";
+    withPath: "";
 }
 
-const Buttons: React.FunctionComponent<ButtonProps> = ({ path, query, button_description, title, children = null, additional_styles = '', ...buttonProps }) => {
-    return (
+const Buttons: React.FunctionComponent<ButtonProps> = ({ path, query, button_description, title, withPath, children = null, additional_styles = '', ...buttonProps }) => {
+    return ((withPath || path) ? (
         <div className={`${buttonProps.disabled ? "pointer-events-none text-black/[.5]" : ""} w-fit select-none`}>
             <Link href={{
                 pathname: buttonProps.disabled ? "" : path,
@@ -22,7 +23,14 @@ const Buttons: React.FunctionComponent<ButtonProps> = ({ path, query, button_des
                 </button>
             </Link>
         </div>
-    )
+    ) : (
+        <div className={`${buttonProps.disabled ? "pointer-events-none text-black/[.5]" : ""} w-fit select-none`}>
+            <button title={title ? title : button_description} className={twMerge(`flex items-center space-x-2 px-5 py-2 rounded-lg bg-gray-200 hover:bg-gray-300 transition-all`, additional_styles)} {...buttonProps}>
+                {children ? <div>{children}</div> : null}
+                {button_description ? <p>{button_description}</p> : null}
+            </button>
+        </div>
+    ))
 }
 
 export default Buttons
