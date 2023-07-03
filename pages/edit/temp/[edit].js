961dd87621a78e476e38903a97f52ed6a85b85f7
karelbondan <karel.herdito@binus.ac.id>
2023-07-03 19:12:51 +0700

    added save and exit button; added loading bar; changed from highlight to native code component

--- pages/edit/temp/[edit].js
+++ pages/edit/temp/[edit].js
@@ -41,6 +41,7 @@ const DocEditor = ({workspace_name, setTitle, config}) => {
       // If there are unsaved changes, prompt the user with a warning dialog
       if (url === router.asPath || !window.confirm(warningText)) {
         // Emit a routeChangeError event and throw an error to prevent navigation away from the page
+        history.go(1);
         router.events.emit('routeChangeError');
         throw 'routeChange aborted.';
       }
@@ -51,19 +52,21 @@ const DocEditor = ({workspace_name, setTitle, config}) => {
 
   useEffect(() => {
     window.addEventListener('beforeunload', handleWindowClose);
-    router.events.on('routeChangeStart', handleBrowseAway);
+    router.events.on('beforeHistoryChange', handleBrowseAway);
     return () => {
       window.removeEventListener('beforeunload', handleWindowClose);
-      router.events.off('routeChangeStart', handleBrowseAway);
+      router.events.off('beforeHistoryChange', handleBrowseAway);
     };
-  }, [IsSaved]);
+  }, [IsSaved, router]);
 
   // This useEffect hook sets up the initial data for the workspace based on the workspace name and form type
   useEffect(() => {
     // Call init_data to set up the workspace data
     const init_call = async () => {
       try {
-        console.log(router.query.workspace_data);
+        // console.log(router.query.workspace_data);
+        await delay(500);
+        router.events.emit('routeChangeStart');
         const initial_data = await init_data(config, router, {
           afe_number: router?.query?.workspace_data,
         });
@@ -87,19 +90,25 @@ const DocEditor = ({workspace_name, setTitle, config}) => {
       }
     };
     init_call();
-  }, []);
+  }, [router.isReady]);
 
   useEffect(() => {
     if (spreadsheetReady) {
       setTimeout(async () => {
         setMessage({
           message:
-            'Please use DD-MM-YYYY format in any date field. You can set the date formatting by going to Format > Number and selecting the correct date format if the field insisted on inputting wrong date format.',
+            'Please use DD/MM/YYYY format in any date field. You can set the date formatting by going to Format > Number and selecting the correct date format if the field insisted on inputting wrong date format.',
           color: 'blue',
           show: true,
         });
         await delay(10000);
-        setMessage({message: '', color: '', show: false});
+        setMessage(x => {
+          return {...x, show: false};
+        });
+        await delay(300);
+        setMessage(x => {
+          return {...x, message: '', color: ''};
+        });
       }, 3000);
     }
   }, [spreadsheetReady]);
@@ -126,8 +135,24 @@ const DocEditor = ({workspace_name, setTitle, config}) => {
           show: true,
         });
         router.events.emit('routeChangeComplete');
-        await delay(3000);
-        setMessage({message: '', color: '', show: false});
+        if (triggerSave.includes('redirect')) {
+          await delay(1000);
+          setMessage({
+            message: 'Redirecting back to record list...',
+            color: 'blue',
+            show: true,
+          });
+          router.back();
+        } else {
+          await delay(3000);
+          setMessage(x => {
+            return {...x, show: false};
+          });
+          await delay(500);
+          setMessage(x => {
+            return {...x, message: '', color: ''};
+          });
+        }
       }
     } catch (error) {
       // Handle error and display error message
@@ -175,7 +200,7 @@ const DocEditor = ({workspace_name, setTitle, config}) => {
     if (triggerSave && IsSaved) {
       if (triggerSave === 'download') {
         downloadWorkspaceHandler();
-      } else if (triggerSave === 'save') {
+      } else if (triggerSave.includes('save')) {
         saveDocumentHandler();
       }
     }
@@ -191,17 +216,32 @@ const DocEditor = ({workspace_name, setTitle, config}) => {
     console.log(workspaceData);
   };
 
+  useEffect(() => {
+    // if all is true, meaning all loading has been finished
+    if ([Data, dataContentDetails, spreadsheetReady].every(x => x)) {
+      router.events.emit('routeChangeComplete');
+    }
+  }, [Data, dataContentDetails, spreadsheetReady]);
+
   return error ? (
     <div className="w-full h-full flex flex-col p-10 space-y-4">
       <p className="font-bold text-lg text-red-500">
         Something happened. Please try again or contact administrator/maintainer
         if the problem still persists by giving them the information below:
       </p>
-      <Highlight className="html rounded-md border-2">{error}</Highlight>
-      <Button
-        path="/datatype/well_data/printed_well_report"
-        button_description="Go back"
-      />
+      <code className="rounded-md border-2 p-2">{error}</code>
+      <div className="flex space-x-2">
+        <Button path="/datatype/well_data/printed_well_report">Go back</Button>
+        <Button
+          path="/datatype/well_data/printed_well_report"
+          onClick={e => {
+            e.preventDefault();
+            setIsSaved(true);
+            router.reload();
+          }}>
+          Reload
+        </Button>
+      </div>
     </div>
   ) : Data[0] !== -1 && dataContentDetails[0] !== -1 ? (
     <Container additional_class="space-y-3">
@@ -343,27 +383,104 @@ const DocEditor = ({workspace_name, setTitle, config}) => {
       </div>
       <div className="flex space-x-2 w-full pt-5">
         <Button
-          path=""
-          button_description="Save record"
           onClick={e => {
             e.preventDefault();
             setIsSaved(true);
             settriggerSave('save');
           }}
-          additional_styles="bg-searchbg/[.6] hover:bg-searchbg font-semibold"
-          disabled={!spreadsheetReady || Message.message ? true : false}
-        />
+          additional_styles="bg-searchbg/[.6] hover:bg-searchbg font-semibold w-[200px] min-w-max justify-center"
+          disabled={!spreadsheetReady || Message.message ? true : false}>
+          <div className="flex space-x-2 items-center">
+            <svg
+              width="18"
+              height="18"
+              stroke-width="1.5"
+              viewBox="0 0 24 24"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg">
+              <path
+                d="M3 19V5C3 3.89543 3.89543 3 5 3H16.1716C16.702 3 17.2107 3.21071 17.5858 3.58579L20.4142 6.41421C20.7893 6.78929 21 7.29799 21 7.82843V19C21 20.1046 20.1046 21 19 21H5C3.89543 21 3 20.1046 3 19Z"
+                stroke="currentColor"
+                stroke-width="1.5"
+              />
+              <path
+                d="M8.6 9H15.4C15.7314 9 16 8.73137 16 8.4V3.6C16 3.26863 15.7314 3 15.4 3H8.6C8.26863 3 8 3.26863 8 3.6V8.4C8 8.73137 8.26863 9 8.6 9Z"
+                stroke="currentColor"
+                stroke-width="1.5"
+              />
+              <path
+                d="M6 13.6V21H18V13.6C18 13.2686 17.7314 13 17.4 13H6.6C6.26863 13 6 13.2686 6 13.6Z"
+                stroke="currentColor"
+                stroke-width="1.5"
+              />
+            </svg>
+            <p>Save changes</p>
+          </div>
+        </Button>
         <Button
-          path=""
-          button_description="Download record"
+          additional_styles="bg-searchbg/[.6] hover:bg-searchbg font-semibold w-[200px] min-w-max justify-center"
+          onClick={e => {
+            e.preventDefault();
+            setIsSaved(true);
+            settriggerSave('save_redirect');
+          }}
+          disabled={
+            !spreadsheetId || Message.message || !spreadsheetReady
+              ? true
+              : false
+          }>
+          <div className="flex space-x-2 items-center">
+            <svg
+              width="18"
+              height="18"
+              stroke-width="1.5"
+              viewBox="0 0 24 24"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg">
+              <path
+                d="M3 19V5C3 3.89543 3.89543 3 5 3H16.1716C16.702 3 17.2107 3.21071 17.5858 3.58579L20.4142 6.41421C20.7893 6.78929 21 7.29799 21 7.82843V19C21 20.1046 20.1046 21 19 21H5C3.89543 21 3 20.1046 3 19Z"
+                stroke="currentColor"
+                stroke-width="1.5"
+              />
+              <path
+                d="M8.6 9H15.4C15.7314 9 16 8.73137 16 8.4V3.6C16 3.26863 15.7314 3 15.4 3H8.6C8.26863 3 8 3.26863 8 3.6V8.4C8 8.73137 8.26863 9 8.6 9Z"
+                stroke="currentColor"
+                stroke-width="1.5"
+              />
+              <path
+                d="M6 13.6V21H18V13.6C18 13.2686 17.7314 13 17.4 13H6.6C6.26863 13 6 13.2686 6 13.6Z"
+                stroke="currentColor"
+                stroke-width="1.5"
+              />
+            </svg>
+            <p>Save and exit</p>
+          </div>
+        </Button>
+        <Button
           onClick={e => {
             e.preventDefault();
             setIsSaved(true);
             settriggerSave('download');
           }}
-          additional_styles="bg-searchbg/[.6] hover:bg-searchbg font-semibold"
-          disabled={!spreadsheetReady || Message.message ? true : false}
-        />
+          additional_styles="bg-searchbg/[.6] hover:bg-searchbg font-semibold w-[200px] min-w-max justify-center"
+          disabled={!spreadsheetReady || Message.message ? true : false}>
+          <div className="flex space-x-2 items-center">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              stroke="currentColor"
+              className="w-5 h-5">
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M9 13.5l3 3m0 0l3-3m-3 3v-6m1.06-4.19l-2.12-2.12a1.5 1.5 0 00-1.061-.44H4.5A2.25 2.25 0 002.25 6v12a2.25 2.25 0 002.25 2.25h15A2.25 2.25 0 0021.75 18V9a2.25 2.25 0 00-2.25-2.25h-5.379a1.5 1.5 0 01-1.06-.44z"
+              />
+            </svg>
+            <p>Download record</p>
+          </div>
+        </Button>
       </div>
       <Toast message={Message} setmessage={setMessage}>
         {Message.message}
