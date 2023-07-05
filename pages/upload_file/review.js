29c36d659737cab8e99bd93fe856934f4d78fe33
karelbondan <karel.herdito@binus.ac.id>
2023-07-05 17:23:54 +0700

    added button icon

--- pages/upload_file/review.js
+++ pages/upload_file/review.js
@@ -268,7 +268,6 @@ export default function UploadFileReview({setTitle, config}) {
                 <Buttons
                   button_description="Hide image"
                   additional_styles="bg-white"
-                  path=""
                   onClick={e => {
                     e.preventDefault();
                     setImageReview('');
@@ -278,7 +277,6 @@ export default function UploadFileReview({setTitle, config}) {
                 <Buttons
                   button_description="View uploaded picture below"
                   additional_styles="bg-white"
-                  path=""
                   onClick={e => {
                     e.preventDefault();
                     setImageReview(ImageURL);
@@ -323,7 +321,6 @@ export default function UploadFileReview({setTitle, config}) {
                 <Buttons
                   button_description="Hide image"
                   additional_styles="bg-white"
-                  path=""
                   onClick={e => {
                     setImageReview('');
                   }}
@@ -343,7 +340,6 @@ export default function UploadFileReview({setTitle, config}) {
         <div className="flex items-center justify-center sticky bottom-2 my-4 z-[10000] w-full pointer-events-none">
           <div className="w-fit flex space-x-2 items-center justify-center bg-white rounded-lg p-2 border pointer-events-auto">
             <Buttons
-              path=""
               title="Previous page"
               button_description=""
               additional_styles="bg-white border-2 p-3 hover:bg-gray-200"
@@ -364,7 +360,6 @@ export default function UploadFileReview({setTitle, config}) {
               <p className="w-5 h-5">{PageNo + 1}</p>
             </div>
             <Buttons
-              path=""
               title="Next page"
               button_description=""
               additional_styles="bg-white border-2 p-3 hover:bg-gray-200"
@@ -386,19 +381,42 @@ export default function UploadFileReview({setTitle, config}) {
       )}
       <div className="flex space-x-3 py-4">
         <Buttons
-          path=""
-          additional_styles="bg-searchbg/[.6] hover:bg-searchbg font-semibold"
+          additional_styles="bg-searchbg/[.6] hover:bg-searchbg font-semibold w-[200px] justify-center"
           onClick={saveDocumentHandler}
           disabled={
             !spreadsheetID || Message.message || !spreadsheetReady
               ? true
               : false
           }>
-          Save changes
+          <div className="flex space-x-2 items-center">
+            <svg
+              width="18"
+              height="18"
+              stroke-width="1.5"
+              viewBox="0 0 24 24"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg">
+              <path
+                d="M3 19V5C3 3.89543 3.89543 3 5 3H16.1716C16.702 3 17.2107 3.21071 17.5858 3.58579L20.4142 6.41421C20.7893 6.78929 21 7.29799 21 7.82843V19C21 20.1046 20.1046 21 19 21H5C3.89543 21 3 20.1046 3 19Z"
+                stroke="currentColor"
+                stroke-width="1.5"
+              />
+              <path
+                d="M8.6 9H15.4C15.7314 9 16 8.73137 16 8.4V3.6C16 3.26863 15.7314 3 15.4 3H8.6C8.26863 3 8 3.26863 8 3.6V8.4C8 8.73137 8.26863 9 8.6 9Z"
+                stroke="currentColor"
+                stroke-width="1.5"
+              />
+              <path
+                d="M6 13.6V21H18V13.6C18 13.2686 17.7314 13 17.4 13H6.6C6.26863 13 6 13.2686 6 13.6Z"
+                stroke="currentColor"
+                stroke-width="1.5"
+              />
+            </svg>
+            <p>Save changes</p>
+          </div>
         </Buttons>
         <Buttons
-          path=""
-          additional_styles="bg-searchbg/[.6] hover:bg-searchbg font-semibold"
+          additional_styles="bg-searchbg/[.6] hover:bg-searchbg font-semibold w-[200px] justify-center"
           onClick={e => {
             saveDocumentHandler(e, true);
           }}
@@ -407,7 +425,32 @@ export default function UploadFileReview({setTitle, config}) {
               ? true
               : false
           }>
-          Save and exit
+          <div className="flex space-x-2 items-center">
+            <svg
+              width="18"
+              height="18"
+              stroke-width="1.5"
+              viewBox="0 0 24 24"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg">
+              <path
+                d="M3 19V5C3 3.89543 3.89543 3 5 3H16.1716C16.702 3 17.2107 3.21071 17.5858 3.58579L20.4142 6.41421C20.7893 6.78929 21 7.29799 21 7.82843V19C21 20.1046 20.1046 21 19 21H5C3.89543 21 3 20.1046 3 19Z"
+                stroke="currentColor"
+                stroke-width="1.5"
+              />
+              <path
+                d="M8.6 9H15.4C15.7314 9 16 8.73137 16 8.4V3.6C16 3.26863 15.7314 3 15.4 3H8.6C8.26863 3 8 3.26863 8 3.6V8.4C8 8.73137 8.26863 9 8.6 9Z"
+                stroke="currentColor"
+                stroke-width="1.5"
+              />
+              <path
+                d="M6 13.6V21H18V13.6C18 13.2686 17.7314 13 17.4 13H6.6C6.26863 13 6 13.2686 6 13.6Z"
+                stroke="currentColor"
+                stroke-width="1.5"
+              />
+            </svg>
+            <p>Save and exit</p>
+          </div>
         </Buttons>
       </div>
       <Toast message={Message} setmessage={setMessage}>
