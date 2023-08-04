059a0a5beb813579e5c32b3363bd3852b83312c0
karelbondan <karel.herdito@binus.ac.id>
2023-08-05 02:08:46 +1000

    added animation, close button and adjusted button color for consistency

--- components/popup/index.tsx
+++ components/popup/index.tsx
@@ -1,18 +1,19 @@
 import {useContext} from 'react';
 import {PopupContext} from '@contexts/PopupContext';
 import Button from '@components/button';
+import CloseThin from '../../public/icons/close-thin.svg';
 
 const Popup = () => {
   let {closePopup, popupProps} = useContext(PopupContext);
-  let {title, message, onConfirm} = popupProps;
+  let {title, message, onConfirm, togglePopup} = popupProps;
 
   const confirm = () => {
     onConfirm();
     closePopup();
   };
   return (
-    <div className="fixed top-0 left-0 flex items-center justify-center w-screen h-screen bg-black/[.2]" onClick={closePopup}>
-      <div className="h-auto min-h-[170px] w-500p bg-white border-gray-300 border-2 rounded-md px-6 py-5" onClick={(e) => e.stopPropagation()}>
+    <div className={`${togglePopup ? 'opacity-100 visible' : 'opacity-0 invisible'} fixed top-0 left-0 flex items-center justify-center w-screen h-screen bg-black/[.2] transition-all`} onClick={closePopup}>
+      <div className={`${togglePopup ? '' : '-translate-y-5 opacity-0'} relative h-auto min-h-[170px] w-500p bg-white border-gray-300 border-2 rounded-md px-6 py-5 transition-all`} onClick={(e) => e.stopPropagation()}>
         <div className="font-bold text-lg">{title}</div>
         <div className="border-b border-b-gray-300" />
         <div className="mt-3 w-full text-center">{message}</div>
@@ -21,8 +22,14 @@ const Popup = () => {
           <Button
             button_description="Confirm"
             onClick={confirm}
-            additional_styles="bg-primary/[.8] hover:bg-primary"
+            additional_styles="bg-searchbg/[.6] hover:bg-searchbg"
           />
+          <Button
+              additional_styles="absolute top-2 right-2 px-1 py-1 text-black"
+              title="Close"
+              onClick={closePopup}>
+              <CloseThin className="w-5 h-5" />
+            </Button>
         </div>
       </div>
     </div>
