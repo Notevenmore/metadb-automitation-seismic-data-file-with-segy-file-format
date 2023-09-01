b3e13b738015095296ed1bfb34ec6c0a2d765fe5
karelbondan <karel.herdito@binus.ac.id>
2023-09-02 02:03:10 +1000

    added types

--- utils/types.tsx
+++ utils/types.tsx
@@ -1,17 +1,33 @@
 export interface MatchingTable {
-    [key: string]: {
-        id: number; 
-        key: string; 
-        value: string | number;
-    }
+  [key: string]: {
+    id: number;
+    key: string;
+    value: string | number;
+  };
 }
 
 export interface HeaderResponse {
-    status: number;
-    response: String[];
+  status: number;
+  response: String[];
 }
 
 export interface DeleteToggle {
   show: boolean;
   afe_number: number;
+}
+
+export interface DatatypeConfig {
+  [key: string]: {
+    workspace: string;
+    afe: string;
+    view: string;
+    workspace_holder_key: string;
+    column_binder: string;
+  };
+}
+
+export interface ServicesConfig {
+  services: {
+    sheets: string;
+  };
 }
